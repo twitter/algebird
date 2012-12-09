21a4c881c3aa2ea87d28496b0f71fb6cd4dd4092
Argyris Zymnis <argyris@twitter.com>
2012-12-09 11:36:26 -0800

    Fixing up tests.

--- src/test/scala/com/twitter/algebird/MetricProperties.scala
+++ src/test/scala/com/twitter/algebird/MetricProperties.scala
@@ -20,17 +20,41 @@ import org.scalacheck._
 import org.scalacheck.Prop._
 
 object BaseMetricProperties extends Properties("Metric") with MetricProperties {
-  property("double metric") = metricLaws[Double]
-  property("int metric") = metricLaws[Int]
-  property("float metric") = metricLaws[Float]
-  property("long metric") = metricLaws[Long]
-  property("short metric") = metricLaws[Short]
+  property("double metric") = metricLaws[Double](defaultEqFn)
+  property("int metric") = metricLaws[Int](defaultEqFn)
+  property("float metric") = metricLaws[Float](defaultEqFn)
+  property("long metric") = metricLaws[Long](defaultEqFn)
+  property("short metric") = metricLaws[Short](defaultEqFn)
 
   implicit val iterMetric = Metric.L1Iterable[Double]
-  property("double iterable metric") = metricLaws[List[Double]]
+
+  // TODO: we won't need this when we have an Equatable trait
+  def listEqfn(a: List[Double], b: List[Double]) = {
+    val maxSize = scala.math.max(a.size, b.size)
+    val diffA = maxSize - a.size
+    val diffB = maxSize - b.size
+    val newA = if(diffA > 0) a ++ Iterator.fill(diffA)(0.0) else a
+    val newB = if(diffB > 0) b ++ Iterator.fill(diffB)(0.0) else b
+    newA == newB
+  }
+
+  property("double iterable metric") = metricLaws[List[Double]](listEqfn)
 
   implicit val mapMetric = Metric.L1Map[Int, Double]
-  property("int double map metric") = metricLaws[Map[Int, Double]]
+
+  // TODO: we won't need this when we have an Equatable trait
+  def mapEqFn(a: Map[Int, Double], b: Map[Int, Double]) = {
+    (a.keySet ++ b.keySet).forall { key =>
+      (a.get(key), b.get(key)) match {
+        case (Some(aVal), Some(bVal)) => aVal == bVal
+        case (Some(aVal), None) => aVal == 0.0
+        case (None, Some(bVal)) => bVal == 0.0
+        case _ => true
+      }
+    }
+  }
+
+  property("int double map metric") = metricLaws[Map[Int, Double]](mapEqFn)
 }
 
 trait MetricProperties {
@@ -38,8 +62,8 @@ trait MetricProperties {
     val m = Metric(a, b)
     beGreaterThan(m, 0.0) || beCloseTo(m, 0.0)
   }
-  def isEqualIffZero[T : Metric : Arbitrary] = forAll { (a: T, b: T) =>
-    beCloseTo(Metric(a, b), 0.0) || a != b
+  def isEqualIffZero[T : Metric : Arbitrary](eqfn: (T, T) => Boolean) = forAll { (a: T, b: T) =>
+    if(eqfn(a, b)) beCloseTo(Metric(a, b), 0.0) else !beCloseTo(Metric(a, b), 0.0)
   }
   def isSymmetric[T : Metric : Arbitrary] = forAll { (a: T, b: T) =>
     beCloseTo(Metric(a, b), Metric(b, a))
@@ -50,10 +74,11 @@ trait MetricProperties {
     beGreaterThan(m1, m2) || beCloseTo(m1, m2)
   }
 
-  def metricLaws[T : Metric : Arbitrary] =
-    isNonNegative[T] && isEqualIffZero[T] && isSymmetric[T] && satisfiesTriangleInequality[T]
+  def metricLaws[T : Metric : Arbitrary](eqfn: (T, T) => Boolean) =
+    isNonNegative[T] && isEqualIffZero[T](eqfn) && isSymmetric[T] && satisfiesTriangleInequality[T]
 
-  // TODO: are there methods in scalacheck that can do these?
-  def beCloseTo(a: Double, b: Double, eps: Double = 1e-6) =  a == b || (math.abs(a - b) / math.abs(a)) < eps || (a.isInfinite && b.isInfinite)
-  def beGreaterThan(a: Double, b: Double, eps: Double = 1e-10) = a > b || a > b - eps || (a.isInfinite && b.isInfinite)
+  // TODO: these are copied elsewhere in the tests. Move them to a common place
+  def beCloseTo(a: Double, b: Double, eps: Double = 1e-10) =  a == b || (math.abs(a - b) / math.abs(a)) < eps || (a.isInfinite && b.isInfinite)
+  def beGreaterThan(a: Double, b: Double, eps: Double = 1e-10) = a > b - eps || (a.isInfinite && b.isInfinite)
+  def defaultEqFn[T](a: T, b: T): Boolean = a == b
 }
