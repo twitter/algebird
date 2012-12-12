b13c1af4c3acdac36704ad8da4fd986b5d6c7384
Oscar Boykin <oscar@twitter.com>
2012-12-12 12:27:49 -0800

    Updates Approximate, improves tests

--- src/main/scala/com/twitter/algebird/Approximate.scala
+++ src/main/scala/com/twitter/algebird/Approximate.scala
@@ -16,61 +16,81 @@ limitations under the License.
 
 package com.twitter.algebird
 
-trait Approximate[T] {
-   implicit def numeric: Numeric[T]
-   // we can assume there is an implicit numeric on T below:
-   def estimate : T
-   def max : T
-   def min : T
-   def probWithinBounds : Double
+// Note the probWithinBounds is a LOWER BOUND (at least this probability)
+case class Approximate[N](min: N, estimate: N, max: N, probWithinBounds: Double)
+  (implicit val numeric: Numeric[N]) {
    // is this value contained within the bounds:
-   def boundsContain(v: T) = numeric.lteq(min, v) && numeric.lteq(v, max)
+   def boundsContain(v: N): Boolean = numeric.lteq(min, v) && numeric.lteq(v, max)
+   /*
+    * Nhis is so you can do: val x = Approximate(1.0, 1.1, 1.2, 0.99)
+    * and then x ~ 1.05 returns true
+    */
+   def ~(v:N): Boolean = boundsContain(v)
    /*
     * Contract is:
     * Prob(boundsContain(estimate)) >= probWithinBounds
     */
-   def isExact: Boolean = numeric.equiv(min, max)
-   def +(right: Approximate[T]): Approximate[T] = {
+   def isExact: Boolean = (probWithinBounds == 1.0) && numeric.equiv(min, max)
+   def +(right: Approximate[N]): Approximate[N] = {
      val n = numeric
      Approximate(n.plus(min, right.min),
        n.plus(estimate, right.estimate),
        n.plus(max, right.max),
        probWithinBounds * right.probWithinBounds)
    }
-   override lazy val toString =
-     "Approximate" + (min, estimate, max, probWithinBounds).toString
-   override def equals(that: Any): Boolean =
-     that.isInstanceOf[Approximate[T]] && {
-       val thatA = that.asInstanceOf[Approximate[T]]
-       (min, estimate, max, probWithinBounds) ==
-         (thatA.min, thatA.estimate, thatA.max, thatA.probWithinBounds)
+   /** This is not distributive, because:
+    * a*(b+c) has two probability multiplications
+    * while (a*b + a*b) has three
+    * Some kind of general formula solver could possibly
+    * make this distributive, but in the mean time, it's only
+    * a group
+    */
+   def *(right: Approximate[N]): Approximate[N] =
+     if(right.isZero || isOne) {
+       right
      }
+     else if(isZero || right.isOne) {
+       this
+     }
+     else {
+       val n = numeric
+       val ends = for(leftv <- List(min, max);
+                      rightv <- List(right.min, right.max))
+                  yield n.times(leftv, rightv)
+
+       val newProb = probWithinBounds * right.probWithinBounds
+
+       Approximate(ends.min, n.times(estimate, right.estimate),
+         ends.max, newProb)
+     }
+
+   def isZero: Boolean = {
+     isExact && numeric.equiv(estimate, numeric.zero)
+   }
+   def isOne: Boolean = {
+     isExact && numeric.equiv(estimate, numeric.one)
+   }
+
+   def negate = {
+    this * Approximate.exact(numeric.negate(numeric.one))
+  }
 }
 
 object Approximate {
-  // Probably most common constructor:
-  def apply[T](mn: T, est: T, mx: T, prob: Double)(implicit n: Numeric[T]) =
-    new Approximate[T] {
-      val numeric = n
-      val estimate = est
-      val max = mx
-      val min = mn
-      val probWithinBounds = prob
-    }
-  def exact[T](v: T)(implicit n: Numeric[T]) =
-    new Approximate[T] {
-      val numeric = n
-      val estimate = v
-      val max = v
-      val min = v
-      val probWithinBounds = 1.0
-    }
-  def zero[N](implicit n: Numeric[N]) = Approximate.exact(n.zero)
-
-  implicit def monoid[N](implicit n: Numeric[N]): Monoid[Approximate[N]] =
+  def exact[N:Numeric](v: N) = Approximate(v,v,v,1.0)
+  def zero[N](implicit n: Numeric[N]) = exact(n.zero)
+  def one[N](implicit n: Numeric[N]) = exact(n.one)
+  // Not a group/ring:
+  // negate fails: x - x != 0, because with some probability the bound is bad.
+  // distributive fails because a*b + a*c ignores that a is either in or out
+  // of the bound, and counts it idependently.
+  implicit def monoid[N](implicit n: Numeric[N]): Monoid[Approximate[N]] = {
+    // avoid capturing the Numeric:
+    val z = Approximate.zero[N]
     new Monoid[Approximate[N]] {
-      lazy val zero = Approximate.zero[N]
-      def plus(left: Approximate[N], right: Approximate[N]) =
-        left + right
+      val zero = z
+      override def isNonZero(v: Approximate[N]) = !v.isZero
+      def plus(left: Approximate[N], right: Approximate[N]) = left + right
     }
+  }
 }
