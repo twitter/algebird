f4a7388e753ae60255c93aacf73d2bea06d763a0
Edwin Chen <edwin@twitter.com>
2012-12-15 05:01:25 -0800

    Add cardinality estimation to Bloom filter.

--- src/main/scala/com/twitter/algebird/BloomFilter.scala
+++ src/main/scala/com/twitter/algebird/BloomFilter.scala
@@ -84,7 +84,11 @@ sealed abstract class BF extends java.io.Serializable{
 
   def + (other: String): BF
 
-  def contains(item: String): Boolean
+  def contains(item: String): ApproximateBoolean
+
+  // Estimates the cardinality of the set of elements that have been
+  // inserted into the Bloom Filter.
+  def size: Approximate[Long]
 }
 
 /**
@@ -97,7 +101,9 @@ case class BFZero(hashes: BFHash, width: Int) extends BF {
 
   def + (other: String) = BFItem(other, hashes, width)
 
-  def contains(item: String) = false
+  def contains(item: String) = ApproximateBoolean.exactFalse
+
+  def size = Approximate.exact[Long](0)
 }
 
 /**
@@ -119,7 +125,9 @@ case class BFItem(item: String, hashes: BFHash, width: Int) extends BF {
 
   def + (other: String) = this ++ BFItem(other, hashes, width)
 
-  def contains(x: String) = item == x
+  def contains(x: String) = ApproximateBoolean.exact(item == x)
+
+  def size = Approximate.exact[Long](1)
 }
 
 /*
@@ -155,10 +163,83 @@ case class BFInstance(hashes : BFHash, bits: BitSet, width: Int) extends BF {
 
   def contains(item: String) = {
     val otherBits = BitSet(hashes(item) : _*)
-    // do this in case hashing function results in < numHashes number of unique bits
-    (bits & otherBits) == otherBits
+
+    if ((bits & otherBits) == otherBits) {
+      // The false positive probability (the probability that the Bloom filter erroneously
+      // claims that an element x is in the set when x is not) is roughly
+      // p = (1 - e^(-numHashes * setCardinality / width))^numHashes
+      // See: http://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
+      //
+      // However, the true set cardinality may not be known. From empirical evidence, though,
+      // it is upper bounded with high probability by 1.1 * estimatedCardinality (as long as the
+      // Bloom filter is not too full), so we plug this into the formula instead.
+      // TODO: investigate this upper bound and density more closely (or derive a better formula).
+      val fpProb =
+        if (density > 0.95) 1.0 // No confidence in the upper bound on cardinality.
+        else scala.math.pow(1 - scala.math.exp(-numHashes * size.estimate * 1.1 / width), numHashes)
+
+      ApproximateBoolean(true, 1 - fpProb)
+    } else {
+      // False negatives are not possible.
+      ApproximateBoolean.exactFalse
+    }
   }
 
+  // Proportion of bits that are set to true.
+  private def density = bits.size.toDouble / width
+
+  /**
+   * Cardinality estimates are taken from Theorem 1 on page 15 of
+   * "Cardinality estimation and dynamic length adaptation for Bloom filters"
+   * by Papapetrou, Siberski, and Nejdl:
+   * http://www.softnet.tuc.gr/~papapetrou/publications/Bloomfilters-DAPD.pdf
+   *
+   * Roughly, by using bounds on the expected number of true bits after n elements
+   * have been inserted into the Bloom filter, we can go from the actual number of
+   * true bits (which is known) to an estimate of the cardinality.
+   *
+   * approximationWidth defines an interval around the maximum-likelihood cardinality
+   * estimate. Namely, the approximation returned is of the form
+   * (min, estimate, max) =
+   *   ((1 - approxWidth) * estimate, estimate, (1 + approxWidth) * estimate)
+   */
+  def size : Approximate[Long] = size(approximationWidth = 0.05)
+  def size(approximationWidth : Double = 0.05) : Approximate[Long] = {
+    assert(0 <= approximationWidth && approximationWidth < 1, "approximationWidth must lie in [0, 1)")
+
+    // Variable names correspond to those used in the paper.
+    val t = bits.size
+    val n = sInverse(t).round.toInt
+    // Take the min and max because the probability formula assumes
+    // nl <= sInverse(t - 1) and sInverse(t + 1) <= nr
+    val nl = scala.math.min(sInverse(t - 1).floor, (1 - approximationWidth) * n).toInt
+    val nr = scala.math.max(sInverse(t + 1).ceil, (1 + approximationWidth) * n).toInt
+    val prob =
+      1 -
+      scala.math.exp(t - 1 - s(nl)) *
+      scala.math.pow(s(nl) / (t - 1), t - 1) -
+      scala.math.exp(-scala.math.pow(t + 1 - s(nr), 2) / (2 * s(nr)))
+
+    Approximate[Long](nl, n, nr, scala.math.max(0, prob))
+  }
+
+  /**
+   * s(n) is the expected number of bits that have been set to true after
+   * n elements have been inserted into the Bloom filter.
+   * This is \hat{S}(n) in the cardinality estimation paper used above.
+   */
+  private def s(n : Int) : Double = {
+    width * (1 - scala.math.pow(1 - 1.0 / width, numHashes * n))
+  }
+
+  /**
+   * sInverse(t) is the maximum likelihood value for the number of distinct
+   * inserted element when the Bloom filter has t bits set to true.
+   * This is \hat{S}^{-1}(n) in the cardinality estimation paper used above.
+   */
+  private def sInverse(t : Int) : Double = {
+    scala.math.log(1 - t.toDouble / width) / (numHashes * scala.math.log(1 - 1.0 / width))
+  }
 
 }
 
