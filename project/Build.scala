19e407189684bd33c781121dbc1fda1a838b476f
Sam Ritchie <sritchie@twitter.com>
2013-02-15 11:43:49 -0800

    adds release plugin

--- project/Build.scala
+++ project/Build.scala
@@ -2,9 +2,10 @@ package algebird
 
 import sbt._
 import Keys._
+import sbtgitflow.ReleasePlugin._
 
 object AlgebirdBuild extends Build {
-  val sharedSettings = Project.defaultSettings ++ Seq(
+  val sharedSettings = Project.defaultSettings ++ releaseSettings ++ Seq(
     organization := "com.twitter",
     version := "0.1.9-SNAPSHOT",
     scalaVersion := "2.9.2",
