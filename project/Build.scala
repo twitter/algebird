f31c940a286d54045338692e34f53fc0f9a8d52f
Sam Ritchie <sritchie@twitter.com>
2013-02-15 11:51:37 -0800

    Move version to version file.

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,6 @@ import sbtgitflow.ReleasePlugin._
 object AlgebirdBuild extends Build {
   val sharedSettings = Project.defaultSettings ++ releaseSettings ++ Seq(
     organization := "com.twitter",
-    version := "0.1.9-SNAPSHOT",
     scalaVersion := "2.9.2",
     crossScalaVersions := Seq("2.9.2", "2.10.0"),
 
