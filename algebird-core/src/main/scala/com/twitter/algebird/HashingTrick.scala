eaa162a6a5a6bd4942c1e905d793c5ee94e1b52a
Avi Bryant <avi@avibryant.com>
2013-04-08 15:05:36 -0700

    Update API to HashingTrick.init

--- algebird-core/src/main/scala/com/twitter/algebird/HashingTrick.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/HashingTrick.scala
@@ -1,3 +1,19 @@
+/*
+Copyright 2013 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.twitter.algebird
 
 class HashingTrickMonoid[V:Group](bits : Int, seed : Int = 123456) extends Monoid[AdaptiveVector[V]] {
@@ -9,12 +25,12 @@ class HashingTrickMonoid[V:Group](bits : Int, seed : Int = 123456) extends Monoi
 
 	def plus(left : AdaptiveVector[V], right : AdaptiveVector[V]) = Monoid.plus(left, right)
 
-	def init(key : Array[Byte], value : V) = {
-		val (long1, long2) = hash(key)
+	def init[K <% Array[Byte]](kv : (K,V)) : AdaptiveVector[V] = {
+		val (long1, long2) = hash(kv._1)
 		val index = (long1 & bitMask).toInt
 		val isNegative = (long2 & 1) == 1
 
-		val signedValue = if(isNegative) Group.negate(value) else value
+		val signedValue = if(isNegative) Group.negate(kv._2) else kv._2
 		AdaptiveVector.fromMap[V](Map(index -> signedValue), Monoid.zero[V], vectorSize)
 	}
 }
