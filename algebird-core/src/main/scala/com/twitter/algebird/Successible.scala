d556c2a0b32e8dd41bc6a37a8ee187eee6ea0e35
Jonathan Coveney <jcoveney@twitter.com>
2013-11-08 10:31:37 -0800

    Make it a bit weaker, rename to Successible

--- algebird-core/src/main/scala/com/twitter/algebird/Successible.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Successible.scala
@@ -19,38 +19,23 @@ package com.twitter.algebird
  * This is a monoid to represent things which increase. Note that it is important
  * that a value after being incremented is always larger than it was before.
  */
-trait Incrementable[@specialized(Int,Long,Float,Double) T] extends Monoid[T] {
-  def one: T
-  def increment(old: T) = plus(old, one)
+trait Successible[@specialized(Int,Long,Float,Double) T] extends Monoid[T] {
+  def next(old: T): T
   def ordering: Ordering[T]
 }
 
-object Incrementable {
-  implicit val intIncrementable = new Incrementable[Int] {
+object Successible {
+  implicit val intSuccessible = new Successible[Int]  {
     val zero = Monoid.intMonoid.zero
     def plus(left: Int, right: Int) = Monoid.intMonoid.plus(left, right)
-    val one = 1
+    def next(old: Int) = old + 1
     val ordering = Ordering.Int
   }
 
-  implicit val longIncrementable = new Incrementable[Long] {
+  implicit val longSuccessible = new Successible[Long] {
     val zero = Monoid.longMonoid.zero
     def plus(left: Long, right: Long) = Monoid.longMonoid.plus(left, right)
-    val one = 1L
+    def next(old: Long) = old + 1
     val ordering = Ordering.Long
-  }
-
-  implicit val floatIncrementable = new Incrementable[Float] {
-    val zero = Monoid.floatMonoid.zero
-    def plus(left: Float, right: Float) = Monoid.floatMonoid.plus(left, right)
-    val one = 1.0f
-    val ordering = Ordering.Float
-  }
-
-  implicit val doubleIncrementable = new Incrementable[Double] {
-    val zero = Monoid.doubleMonoid.zero
-    def plus(left: Double, right: Double) = Monoid.doubleMonoid.plus(left, right)
-    val one = 1.0
-    val ordering = Ordering.Double
   }
 }
---
+++ algebird-test/src/test/scala/com/twitter/algebird/SuccessibleProperties.scala
@@ -0,0 +1,44 @@
+/*
+Copyright 2012 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.twitter.algebird
+
+import java.util.Random
+
+import org.scalacheck.{ Arbitrary, Properties }
+import org.scalacheck.Prop.forAll
+
+import scala.annotation.tailrec
+
+object SuccessibleProperties extends Properties("Successibles") {
+  import BaseProperties._
+  import Successible._
+
+  def alwaysIncreasing[T: Successible] = {
+    val r = new Random
+    val incrementable = implicitly[Successible[T]]
+    val start = incrementable.zero
+    val ord = incrementable.ordering
+    1.to(100).foldLeft((start, true)) { case ((oldVal, hasBeenGreater), _) =>
+      val newVal = incrementable.next(oldVal)
+      (newVal, hasBeenGreater && ord.compare(oldVal, newVal) < 0)
+    }._2
+  }
+
+  def successibleLaws[T: Successible: Arbitrary] = monoidLaws[T] && alwaysIncreasing[T]
+
+  property("Successible[Int] is an Successible") = successibleLaws[Int]
+  property("Successible[Long] is an Successible") = successibleLaws[Long]
+}
