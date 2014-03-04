990535bbb84cdb2f332f42800f6d245aaafcd910
Ian O Connell <ioconnell@twitter.com>
2014-03-04 13:48:33 -0800

    Remove unused constants, whitespace changes

--- algebird-core/src/main/scala/com/twitter/algebird/HyperLogLog.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/HyperLogLog.scala
@@ -88,7 +88,7 @@ object HyperLogLog {
     loop(0, 0)
   }
 
-  /** The value 'w' is equal to <w_bits ... w_n>. The function rho counts the number of leading 
+  /** The value 'w' is equal to <w_bits ... w_n>. The function rho counts the number of leading
    *  zeroes in 'w'. We can calculate rho(w) at once with the method rhoW.
    */
   def rhoW(bsl: BitSetLite, bits: Int): Byte = {
@@ -100,10 +100,10 @@ object HyperLogLog {
   }
 
   /** We are computing j and \rho(w) from the paper,
-   *  sorry for the name, but it allows someone to compare to the paper extremely low probability 
+   *  sorry for the name, but it allows someone to compare to the paper extremely low probability
    *  rhow (position of the leftmost one bit) is > 127, so we use a Byte to store it
-   *  Given a hash <w_0, w_1, w_2 ... w_n> the value 'j' is equal to <w_0, w_1 ... w_(bits-1)> and 
-   *  the value 'w' is equal to <w_bits ... w_n>. The function rho counts the number of leading 
+   *  Given a hash <w_0, w_1, w_2 ... w_n> the value 'j' is equal to <w_0, w_1 ... w_(bits-1)> and
+   *  the value 'w' is equal to <w_bits ... w_n>. The function rho counts the number of leading
    *  zeroes in 'w'. We can calculate rho(w) at once with the method rhoW.
    */
   def jRhoW(in: Array[Byte], bits: Int): (Int, Byte) = {
@@ -184,10 +184,6 @@ object HyperLogLog {
   }
 
   def error(bits: Int): Double = 1.04/scala.math.sqrt(twopow(bits))
-
-  // Some constants from the algorithm:
-  private[algebird] val fourBillionSome = twopow(32)
-  private[algebird] val largeE = fourBillionSome/30.0
 }
 
 sealed abstract class HLL extends java.io.Serializable {
