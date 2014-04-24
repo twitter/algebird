a6a754232a0147c52f00b47c289da2d369679edf
julien <julien@twitter.com>
2014-04-24 11:48:34 -0700

    make SummingCache a trait and provide multiple implementations that can be picked based on the monoid implementation

--- algebird-core/src/main/scala/com/twitter/algebird/SummingCache.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/SummingCache.scala
@@ -20,20 +20,37 @@ package com.twitter.algebird
  * @author Oscar Boykin
  */
 
-import java.util.concurrent.ArrayBlockingQueue
+import java.util.{LinkedHashMap => JLinkedHashMap}
+import java.util.{Map => JMap}
 
-import java.util.{ LinkedHashMap => JLinkedHashMap, Map => JMap }
+import scala.Option.option2Iterable
+import scala.annotation.implicitNotFound
+import scala.collection.JavaConverters.mapAsScalaMapConverter
+import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.{Map => MMap}
-import scala.collection.JavaConverters._
-import scala.annotation.tailrec
 
 object SummingCache {
-  def apply[K,V:Semigroup](cap: Int): SummingCache[K,V] = new SummingCache[K,V](cap)
+  /**
+   * returns the default SummingCache implementation: PlusOptimizedSummingCache
+   */
+  def apply[K,V:Semigroup](cap: Int): SummingCache[K,V] = PlusOptimizedSummingCache(cap)
 }
+
+object SumOptionOptimizedSummingCache {
+  def apply[K,V:Semigroup](cap: Int): SummingCache[K,V] = new SumOptionOptimizedSummingCache[K,V](cap)
+}
+
+object PlusOptimizedSummingCache {
+  def apply[K,V:Semigroup](cap: Int): SummingCache[K,V] = new PlusOptimizedSummingCache[K,V](cap)
+}
+
+trait SummingCache[K,V] extends StatefulSummer[Map[K,V]]
+
 /** A Stateful Summer on Map[K,V] that keeps a cache of recent keys
+ *  @author Oscar Boykin
  */
-class SummingCache[K,V] private (capacity: Int)(implicit sgv: Semigroup[V])
-  extends StatefulSummer[Map[K,V]] {
+class PlusOptimizedSummingCache[K,V] private (capacity: Int)(implicit sgv: Semigroup[V])
+  extends SummingCache[K,V] {
 
   require(capacity >= 0, "Cannot have negative capacity in SummingIterator")
 
@@ -74,4 +91,74 @@ class SummingCache[K,V] private (capacity: Int)(implicit sgv: Semigroup[V])
           false
         }
     }).asScala
+}
+
+
+/** A Stateful Summer on Map[K,V] that keeps a cache of recent keys
+ *  @author Julien Le Dem
+ */
+class SumOptionOptimizedSummingCache[K,V] private (capacity: Int)(implicit sgv: Semigroup[V])
+  extends SummingCache[K,V] {
+
+  require(capacity >= 0, "Cannot have negative capacity in SummingIterator")
+
+  override val semigroup = new MapMonoid[K,V]
+
+  private var presentTuples = 0
+  private val cache = MMap[K, ListBuffer[V]]()
+
+  override def put(m: Map[K,V]): Option[Map[K,V]] = {
+    m.foreach { case (k, v) =>
+        cache.getOrElseUpdate(k, ListBuffer[V]()) += v
+      }
+    presentTuples += m.size
+
+    // if the number of distinct keys fits in the buffer, we just aggregate values
+    if (presentTuples >= capacity && cache.size < capacity) aggregateCache
+
+    // if they don't we need to flush
+    if (presentTuples >= capacity)
+      flush
+    else
+      None
+  }
+
+  private def sum(items: Seq[V]): Option[V] = {
+    if (items.size == 1) {
+      Some(items.head)
+    } else {
+      sgv.sumOption(items)
+    }
+  }
+
+  /**
+   * aggregates each value list in the cache in place
+   */
+  private def aggregateCache() = {
+    var newCount = 0
+    cache.foreach { case (k, listV) =>
+      if (listV.size > 2) { // we don't want to aggregate if the list is not big enough
+        val sum = sgv.sumOption(listV)
+        listV.clear
+        sum.foreach((v) => listV += v)
+      }
+      newCount += listV.size
+    }
+    presentTuples = newCount
+  }
+
+  /**
+   * aggregate and write the cache to the output
+   */
+  override def flush: Option[Map[K,V]] = {
+    val flushed = cache.toMap.flatMap{ case (k, listV) =>
+        sum(listV).map(v => (k, v))
+      }
+     presentTuples = 0
+     cache.clear
+     if (flushed.isEmpty) None else Some(flushed)
+  }
+
+  def isFlushed = presentTuples == 0
+
 }
