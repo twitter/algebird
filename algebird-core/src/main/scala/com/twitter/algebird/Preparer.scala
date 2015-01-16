298df174e3fa963511a79c6a5e6aa0fa72285d2b
Avi Bryant <avi@stripe.com>
2015-01-15 22:08:26 -0800

    rename Preparer subclasses

--- algebird-core/src/main/scala/com/twitter/algebird/Preparer.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Preparer.scala
@@ -3,7 +3,7 @@ package com.twitter.algebird
 trait Preparer[A, T, +This[A, T] <: Preparer[A, T, This]] extends HasMonoidAggregate[A, T] {
   def map[U](fn: T => U): This[A, U]
 
-  def flatMap[U](fn: T => TraversableOnce[U]): FlatPreparer[A, U]
+  def flatMap[U](fn: T => TraversableOnce[U]): FlatMapPreparer[A, U]
 
   //really ought to generate N versions of this...
   def split[B1, B2, C1, C2](fn: This[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
@@ -13,18 +13,18 @@ trait Preparer[A, T, +This[A, T] <: Preparer[A, T, This]] extends HasMonoidAggre
 }
 
 object Preparer {
-  def apply[A]: SimplePreparer[A, A] = SimplePreparer[A, A](identity)
+  def apply[A] = MapPreparer[A, A](identity)
 }
 
-case class SimplePreparer[A, T](prepareFn: A => T)
-  extends Preparer[A, T, SimplePreparer]
+case class MapPreparer[A, T](prepareFn: A => T)
+  extends Preparer[A, T, MapPreparer]
   with HasAggregate[A, T] {
 
   def map[U](fn: T => U) =
-    SimplePreparer[A, U](fn.compose(prepareFn))
+    MapPreparer[A, U](fn.compose(prepareFn))
 
   def flatMap[U](fn: T => TraversableOnce[U]) =
-    FlatPreparer[A, U](fn.compose(prepareFn))
+    FlatMapPreparer[A, U](fn.compose(prepareFn))
 
   def aggregate[B, C](aggregator: Aggregator[T, B, C]): Aggregator[A, B, C] =
     aggregator.composePrepare(prepareFn)
@@ -33,14 +33,14 @@ case class SimplePreparer[A, T](prepareFn: A => T)
     aggregator.composePrepare(prepareFn)
 }
 
-case class FlatPreparer[A, T](prepareFn: A => TraversableOnce[T])
-  extends Preparer[A, T, FlatPreparer]
+case class FlatMapPreparer[A, T](prepareFn: A => TraversableOnce[T])
+  extends Preparer[A, T, FlatMapPreparer]
   with HasLift[A, T] {
   def map[U](fn: T => U) =
-    FlatPreparer { a: A => prepareFn(a).map(fn) }
+    FlatMapPreparer { a: A => prepareFn(a).map(fn) }
 
   def flatMap[U](fn: T => TraversableOnce[U]) =
-    FlatPreparer { a: A => prepareFn(a).flatMap(fn) }
+    FlatMapPreparer { a: A => prepareFn(a).flatMap(fn) }
 
   def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C] =
     aggregator.sumBefore.composePrepare(prepareFn)
