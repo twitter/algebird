d575bc44d6cdeaf5896e8a6c583cab5c63261ec1
Avi Bryant <avi@stripe.com>
2015-01-15 22:05:04 -0800

    Flesh out Preparer

--- algebird-core/src/main/scala/com/twitter/algebird/Preparer.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Preparer.scala
@@ -1,8 +1,27 @@
 package com.twitter.algebird
 
-case class Preparer[A, T](prepareFn: A => T) {
+trait Preparer[A, T, +This[A, T] <: Preparer[A, T, This]] extends HasMonoidAggregate[A, T] {
+  def map[U](fn: T => U): This[A, U]
+
+  def flatMap[U](fn: T => TraversableOnce[U]): FlatPreparer[A, U]
+
+  //really ought to generate N versions of this...
+  def split[B1, B2, C1, C2](fn: This[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
+    val (a1, a2) = fn(this.asInstanceOf[This[A, T]])
+    a1.join(a2)
+  }
+}
+
+object Preparer {
+  def apply[A]: SimplePreparer[A, A] = SimplePreparer[A, A](identity)
+}
+
+case class SimplePreparer[A, T](prepareFn: A => T)
+  extends Preparer[A, T, SimplePreparer]
+  with HasAggregate[A, T] {
+
   def map[U](fn: T => U) =
-    Preparer[A, U](fn.compose(prepareFn))
+    SimplePreparer[A, U](fn.compose(prepareFn))
 
   def flatMap[U](fn: T => TraversableOnce[U]) =
     FlatPreparer[A, U](fn.compose(prepareFn))
@@ -10,28 +29,78 @@ case class Preparer[A, T](prepareFn: A => T) {
   def aggregate[B, C](aggregator: Aggregator[T, B, C]): Aggregator[A, B, C] =
     aggregator.composePrepare(prepareFn)
 
-  def split[B1, B2, C1, C2](fn: Preparer[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
-    val (a1, a2) = fn(this)
-    a1.join(a2)
+  def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C] =
+    aggregator.composePrepare(prepareFn)
+}
+
+case class FlatPreparer[A, T](prepareFn: A => TraversableOnce[T])
+  extends Preparer[A, T, FlatPreparer]
+  with HasLift[A, T] {
+  def map[U](fn: T => U) =
+    FlatPreparer { a: A => prepareFn(a).map(fn) }
+
+  def flatMap[U](fn: T => TraversableOnce[U]) =
+    FlatPreparer { a: A => prepareFn(a).flatMap(fn) }
+
+  def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C] =
+    aggregator.sumBefore.composePrepare(prepareFn)
+
+  //alias for convenience
+  def aggregate[B, C](aggregator: MonoidAggregator[T, B, C]) = monoidAggregate(aggregator)
+}
+
+trait HasMonoidAggregate[A, T] {
+  def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C]
+
+  def count(pred: T => Boolean) = monoidAggregate(Aggregator.count(pred))
+  def exists(pred: T => Boolean) = monoidAggregate(Aggregator.exists(pred))
+  def forall(pred: T => Boolean) = monoidAggregate(Aggregator.forall(pred))
+  def size = monoidAggregate(Aggregator.size)
+
+  def sortedTake(count: Int)(implicit ord: Ordering[T]) =
+    monoidAggregate(Aggregator.sortedTake(count))
+
+  def sortedReverseTake(count: Int)(implicit ord: Ordering[T]) =
+    monoidAggregate(Aggregator.sortedReverseTake(count))
+
+  def toList = monoidAggregate(Aggregator.toList)
+  def toSet = monoidAggregate(Aggregator.toSet)
+  def uniqueCount = monoidAggregate(Aggregator.uniqueCount)
+}
+
+trait HasAggregate[A, T] {
+  def aggregate[B, C](aggregator: Aggregator[T, B, C]): Aggregator[A, B, C]
+
+  def head = aggregate(Aggregator.head)
+  def last = aggregate(Aggregator.last)
+  def max(implicit ord: Ordering[T]) = aggregate(Aggregator.max)
+  def maxBy[U: Ordering](fn: T => U) = {
+    implicit val ordT = Ordering.by(fn)
+    aggregate(Aggregator.max[T])
+  }
+
+  def min(implicit ord: Ordering[T]) = aggregate(Aggregator.min)
+  def minBy[U: Ordering](fn: T => U) = {
+    implicit val ordT = Ordering.by(fn)
+    aggregate(Aggregator.min[T])
   }
 }
 
-object Preparer {
-  def apply[A]: Preparer[A, A] = Preparer[A, A](identity)
-}
+trait HasLift[A, T] extends HasMonoidAggregate[A, T] {
+  def lift[B, C](aggregator: Aggregator[T, B, C]): MonoidAggregator[A, Option[B], Option[C]] =
+    monoidAggregate(aggregator.lift)
 
-case class FlatPreparer[A, T](prepareFn: A => TraversableOnce[T]) {
-  def map[U](fn: T => U): FlatPreparer[A, U] =
-    FlatPreparer { a: A => prepareFn(a).map(fn) }
+  def head = lift(Aggregator.head)
+  def last = lift(Aggregator.last)
+  def max(implicit ord: Ordering[T]) = lift(Aggregator.max)
+  def maxBy[U: Ordering](fn: T => U) = {
+    implicit val ordT = Ordering.by(fn)
+    lift(Aggregator.max[T])
+  }
 
-  def flatMap[U](fn: T => TraversableOnce[U]): FlatPreparer[A, U] =
-    FlatPreparer { a: A => prepareFn(a).flatMap(fn) }
-
-  def aggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C] =
-    aggregator.sumBefore.composePrepare(prepareFn)
-
-  def split[B1, B2, C1, C2](fn: FlatPreparer[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
-    val (a1, a2) = fn(this)
-    a1.join(a2)
+  def min(implicit ord: Ordering[T]) = lift(Aggregator.min)
+  def minBy[U: Ordering](fn: T => U) = {
+    implicit val ordT = Ordering.by(fn)
+    lift(Aggregator.min[T])
   }
 }
