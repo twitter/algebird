370ce2484100cf39035fa98f06faca361ceab0bc
Avi Bryant <avi@stripe.com>
2015-01-18 14:43:46 -0800

    add some uniform random noise to the DecayedValue tests

--- algebird-test/src/test/scala/com/twitter/algebird/DecayedValueTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/DecayedValueTest.scala
@@ -14,22 +14,27 @@ class DecayedValueLaws extends PropSpec with PropertyChecks with Matchers {
 
   def averageApproxEq(fn: (DecayedValue, Params) => Double)(implicit p: Arbitrary[Params]) = {
     forAll{ (params: Params) =>
-      val data = (0 to params.count).map{ t => DecayedValue.build(params.mean, t, params.halfLife) }
+      val rand = new scala.util.Random
+      val data = (0 to params.count).map{ t =>
+        val noise = rand.nextDouble * params.maxNoise * rand.nextInt.signum
+        DecayedValue.build(params.mean + (params.mean * noise), t, params.halfLife)
+      }
       val result = decayedMonoid.sum(data)
       assert(approxEq(fn(result, params), params.mean))
     }
   }
 
   implicit val decayedMonoid = DecayedValue.monoidWithEpsilon(0.001)
-  case class Params(mean: Double, halfLife: Double, count: Int)
+  case class Params(mean: Double, halfLife: Double, count: Int, maxNoise: Double)
 
   property("for large HL and count, average(f(t)=x)=x") {
     implicit val params: Arbitrary[Params] = Arbitrary{
       for (
         x <- choose(-1e100, 1e100);
         hl <- choose(100.0, 1000.0);
-        c <- choose(10000, 100000)
-      ) yield Params(x, hl, c)
+        c <- choose(10000, 100000);
+        n <- choose(0.0, 0.2)
+      ) yield Params(x, hl, c, n)
     }
 
     averageApproxEq{ (dv, params) => dv.average(params.halfLife) }
@@ -40,8 +45,9 @@ class DecayedValueLaws extends PropSpec with PropertyChecks with Matchers {
       for (
         x <- choose(-1e100, 1e100);
         hl <- choose(100.0, 1000.0);
-        c <- choose(20, 1000)
-      ) yield Params(x, hl, c)
+        c <- choose(20, 1000);
+        n <- choose(0.0, 0.2)
+      ) yield Params(x, hl, c, n)
     }
 
     averageApproxEq{ (dv, params) => dv.averageFrom(params.halfLife, 0, params.count) }
@@ -52,8 +58,9 @@ class DecayedValueLaws extends PropSpec with PropertyChecks with Matchers {
       for (
         x <- choose(-1e100, 1e100);
         hl <- choose(1.0, 10.0);
-        c <- choose(10000, 100000)
-      ) yield Params(x, hl, c)
+        c <- choose(10000, 100000);
+        n <- choose(0.0, 0.2)
+      ) yield Params(x, hl, c, n)
     }
 
     averageApproxEq{ (dv, params) => dv.discreteAverage(params.halfLife) }
