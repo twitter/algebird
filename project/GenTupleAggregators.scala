d72e1f10cb9162e45f90dcdb198e122ecf112632
Danielle Sucher <dsucher@gmail.com>
2015-01-30 01:31:54 -0500

    Return a MonoidAggregator from MultiAggregator when possible
    Given MonoidAggregators as input, return a MonoidAggregator
    instead of just a plain old Aggregator.

--- project/GenTupleAggregators.scala
+++ project/GenTupleAggregators.scala
@@ -18,36 +18,40 @@ trait GeneratedTupleAggregator {
 """package com.twitter.algebird
 
 object MultiAggregator {
-""" + genMethods(22, "def", Some("apply")) + "\n" + "}")
+""" +
+      genMethods(22, "def", Some("apply")) + "\n" +
+      genMethods(22, "def", Some("apply"), true) + "\n" + "}")
 
     Seq(tupleAggPlace, multiAggPlace)
   }
 
-  def genMethods(max: Int, defStr: String, name: Option[String]): String = {
+  def genMethods(max: Int, defStr: String, name: Option[String], isMonoid: Boolean = false): String = {
     (2 to max).map(i => {
       val methodName = name.getOrElse("from%d".format(i))
+      val aggType = if (isMonoid) "Monoid" else ""
       val nums = (1 to i)
       val bs = nums.map("B" + _).mkString(", ")
       val cs = nums.map("C" + _).mkString(", ")
-      val aggs = nums.map(x => "Aggregator[A, B%s, C%s]".format(x, x)).mkString(", ")
+      val aggs = nums.map(x => "%sAggregator[A, B%s, C%s]".format(aggType, x, x)).mkString(", ")
       val prepares = nums.map(x => "aggs._%s.prepare(a)".format(x)).mkString(", ")
-      val semigroups = nums.map(x => "aggs._%s.semigroup".format(x)).mkString(", ")
-      val semigroup = "new Tuple%dSemigroup()(%s)".format(i, semigroups)
+      val semiType = if (isMonoid) "monoid" else "semigroup"
+      val semigroups = nums.map(x => "aggs._%s.%s".format(x, semiType)).mkString(", ")
+      val semigroup = "new Tuple%d%s()(%s)".format(i, semiType.capitalize, semigroups)
       val present = nums.map(x => "aggs._%s.present(b._%s)".format(x, x)).mkString(", ")
       val tupleBs = "Tuple%d[%s]".format(i, bs)
       val tupleCs = "Tuple%d[%s]".format(i, cs)
 
       """
-%s %s[A, %s, %s](aggs: Tuple%d[%s]): Aggregator[A, %s, %s] = {
-  new Aggregator[A, %s, %s] {
+%s %s[A, %s, %s](aggs: Tuple%d[%s]): %sAggregator[A, %s, %s] = {
+  new %sAggregator[A, %s, %s] {
     def prepare(a: A) = (%s)
-    val semigroup = %s
+    val %s = %s
     def present(b: %s) = (%s)
   }
-}""".format(defStr, methodName, bs, cs, i, aggs, tupleBs, tupleCs,
-            tupleBs, tupleCs,
+}""".format(defStr, methodName, bs, cs, i, aggs, aggType, tupleBs, tupleCs,
+            aggType, tupleBs, tupleCs,
             prepares,
-            semigroup,
+            semiType, semigroup,
             tupleBs, present)
     }).mkString("\n")
   }
