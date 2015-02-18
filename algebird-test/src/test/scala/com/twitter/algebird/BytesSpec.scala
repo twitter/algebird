7515e815279cbc8d61e5bbd3b230f395879e1c3a
Michael G. Noll <mnoll@verisign.com>
2015-02-18 09:50:42 +0100

    Improve test spec of Bytes, notably to cover inequality

--- algebird-test/src/test/scala/com/twitter/algebird/BytesSpec.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/BytesSpec.scala
@@ -6,6 +6,10 @@ import org.scalatest.{Matchers, WordSpec}
 
 class BytesSpec extends WordSpec with Matchers with GeneratorDrivenPropertyChecks {
 
+  "requires non-null arrays" in {
+    an[IllegalArgumentException] should be thrownBy Bytes(null)
+  }
+
   "has a sane equals function" in {
     val random = new scala.util.Random
     forAll((Gen.choose(1, 4096), "wordLength")) { (wordLength: Int) =>
@@ -28,8 +32,10 @@ class BytesSpec extends WordSpec with Matchers with GeneratorDrivenPropertyCheck
       (bytes1 equals bytes2) should be(bytes2 equals bytes1)
 
       // 3. equals is transitive.
-      val array3 = word.getBytes
-      val bytes3 = Bytes(array3)
+      val bytes3 = {
+        val array3 = word.getBytes
+        Bytes(array3)
+      }
       (bytes1 equals bytes3) should (be(bytes1 equals bytes2) and be(bytes2 equals bytes3))
 
       // 4. equals is consistent.
@@ -41,6 +47,17 @@ class BytesSpec extends WordSpec with Matchers with GeneratorDrivenPropertyCheck
       (bytes1 equals null) should be(false)
       (bytes2 equals null) should be(false)
       (bytes3 equals null) should be(false)
+
+      // 6. equals is false if the contents are different.
+      val bytesSuffixed = {
+        val arraySuffixed = (word + "suffixed").getBytes
+        Bytes(arraySuffixed)
+      }
+      (bytes1 equals bytesSuffixed) should be(false)
+
+      // 7. equals is false if the types are different.
+      (bytes1 equals array1) should be(false)
+      (bytes1 equals new AnyRef) should be(false)
     }
   }
 
@@ -55,8 +72,19 @@ class BytesSpec extends WordSpec with Matchers with GeneratorDrivenPropertyCheck
       val bytes2 = Bytes(array2)
       (bytes1 equals bytes2) should be(true)
 
-      // Then their wrappers should have the same hashCode().
+      // Then their wrappers should have the same hash code.
       bytes1.hashCode should be(bytes2.hashCode)
+
+      // Beyond the standard contract of `hashCode` (which is covered above), we also want small changes to an instance
+      // to result in a different hash code.  The verifications below (combined with the generator-driven test setup)
+      // are a naive smoke test.
+      val appended = Bytes(bytes1.array :+ bytes1.array.head)
+      bytes1.hashCode shouldNot be(appended.hashCode)
+
+      if (bytes1.array.length > 1) {
+        val shortened = Bytes(bytes1.array.tail)
+        bytes1.hashCode shouldNot be(shortened.hashCode)
+      }
     }
   }
 
@@ -78,12 +106,43 @@ class BytesSpec extends WordSpec with Matchers with GeneratorDrivenPropertyCheck
       // Note: We need to combine two ranges because `bytes1 should be(bytes2)` translates into an equality check.
       bytes1 should (be <= bytes2 and be >= bytes2)
 
-      // 3. a Bytes instance and a longer, suffixed Bytes instance.
-      val arraySuffixed = (word + "suffixed").getBytes
-      val bytesSuffixed = Bytes(arraySuffixed)
-      bytes1 should be < bytesSuffixed
-      bytesSuffixed should be > bytes1
+      // 3. a Bytes instance and a "longer" Bytes instance.
+      val suffixed = {
+        val arraySuffixed = (word + "suffixed").getBytes
+        Bytes(arraySuffixed)
+      }
+      bytes1 should be < suffixed
+      suffixed should be > bytes1
+
+      // 4. a Bytes instances and a "shorter" Bytes instance.
+      if (bytes1.array.length > 1) {
+        val shortened = Bytes(bytes1.array.tail)
+        bytes1 should be > shortened
+        shortened should be < bytes1
+      }
     }
+  }
+
+  "forbids, by contract, the mutation of the wrapped array but does not prevent it" in {
+    // This test case documents that we do not put any safeguards in place to prevent mutation of the wrapped array.
+    // Not doing so (e.g. not creating a defensive copy of the array) is a deliberate decision to improve the
+    // performance of Bytes.
+
+    // Given a Bytes instance
+    val bytes = Bytes(Array(1.toByte))
+    val originalHashCode = bytes.hashCode
+    // And a copy of the instance
+    val copy = bytes.copy()
+
+    // When we mutate the wrapped array
+    bytes.array.update(0, 2)
+
+    // Then the new hash code should be different
+    bytes.hashCode shouldNot be(originalHashCode)
+
+    // And the copy of the original Bytes instance is NOT a defensive copy of the instance because it wraps the same
+    // (mutated) array.
+    bytes should be(copy)
   }
 
 }
