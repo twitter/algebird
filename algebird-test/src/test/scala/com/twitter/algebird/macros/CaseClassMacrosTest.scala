eb421469b75971b79c5a00afbb6a09dd205a2df8
Sidharth Kapur <skapur@twitter.com>
2015-07-20 13:52:59 -0700

    Use ScalaCheck only in CaseClassMacrosTest

--- algebird-test/src/test/scala/com/twitter/algebird/macros/CaseClassMacrosTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/macros/CaseClassMacrosTest.scala
@@ -4,11 +4,10 @@ import com.twitter.algebird._
 import com.twitter.algebird.macros.caseclass._
 import com.twitter.algebird.macros.ArbitraryCaseClassMacro.arbitrary
 
-import org.scalatest._
-import org.scalacheck.Arbitrary
-import org.scalatest.prop.{ GeneratorDrivenPropertyChecks, PropertyChecks }
+import org.scalacheck.{ Properties, Arbitrary }
+import org.scalacheck.Prop.forAll
 
-class CaseClassMacrosTest extends PropSpec with PropertyChecks with Matchers {
+object CaseClassMacrosTest extends Properties("Case class macros") {
   import BaseProperties._
 
   implicit val arbitraryFoo: Arbitrary[Foo] = arbitrary[Foo]
@@ -17,29 +16,13 @@ class CaseClassMacrosTest extends PropSpec with PropertyChecks with Matchers {
   case class Foo(a: Int, b: Short, c: Long)
   case class Bar(a: Boolean, foo: Foo)
 
-  property("Foo is a Semigroup") {
-    semigroupLaws[Foo]
-  }
-  property("Foo is a Monoid") {
-    monoidLaws[Foo]
-  }
-  property("Foo is a Group") {
-    groupLaws[Foo]
-  }
-  property("Foo is a Ring") {
-    ringLaws[Foo]
-  }
+  property("Foo is a Semigroup") = semigroupLaws[Foo]
+  property("Foo is a Monoid") = monoidLaws[Foo]
+  property("Foo is a Group") = groupLaws[Foo]
+  property("Foo is a Ring") = ringLaws[Foo]
 
-  property("Bar is a Semigroup") {
-    semigroupLaws[Bar]
-  }
-  property("Bar is a Monoid") {
-    monoidLaws[Bar]
-  }
-  property("Bar is a Group") {
-    groupLaws[Bar]
-  }
-  property("Bar is a Ring") {
-    ringLaws[Bar]
-  }
+  property("Bar is a Semigroup") = semigroupLaws[Bar]
+  property("Bar is a Monoid") = monoidLaws[Bar]
+  property("Bar is a Group") = groupLaws[Bar]
+  property("Bar is a Ring") = ringLaws[Bar]
 }
