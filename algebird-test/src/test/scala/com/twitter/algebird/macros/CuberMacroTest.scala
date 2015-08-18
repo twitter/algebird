39c733a0f098dde3eb5d911f26fbc9b45167a415
Sidharth Kapur <skapur@twitter.com>
2015-08-17 18:04:46 -0700

    Use scalatest instead of scalacheck

--- algebird-test/src/test/scala/com/twitter/algebird/macros/CuberMacroTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/macros/CuberMacroTest.scala
@@ -4,10 +4,12 @@ import com.twitter.algebird._
 import com.twitter.algebird.macros._
 import com.twitter.algebird.macros.ArbitraryCaseClassMacro.arbitrary
 
-import org.scalacheck.{ Properties, Arbitrary }
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Arbitrary
 
-object CuberRollerProperties extends Properties("Cuber and roller macro") {
+import org.scalatest.{ PropSpec, Matchers }
+import org.scalatest.prop.PropertyChecks
+
+class CuberRollerProperties extends PropSpec with PropertyChecks with Matchers {
   import BaseProperties._
 
   implicit val arbitraryFoo: Arbitrary[Foo] = arbitrary[Foo]
@@ -18,50 +20,62 @@ object CuberRollerProperties extends Properties("Cuber and roller macro") {
   case class Bar(a: Boolean, foo: Foo)
   case class Baz(a: Int)
 
-  property("Cuber works for Foo") = forAll { f: Foo =>
-    Cuber.cuber(f) == List(
-      (None, None, None),
-      (Some(f.a), None, None),
-      (None, Some(f.b), None),
-      (Some(f.a), Some(f.b), None),
-      (None, None, Some(f.c)),
-      (Some(f.a), None, Some(f.c)),
-      (None, Some(f.b), Some(f.c)),
-      (Some(f.a), Some(f.b), Some(f.c)))
+  property("Cuber works for Foo"){
+    forAll { f: Foo =>
+      assert(Cuber.cuber(f) == List(
+        (None, None, None),
+        (Some(f.a), None, None),
+        (None, Some(f.b), None),
+        (Some(f.a), Some(f.b), None),
+        (None, None, Some(f.c)),
+        (Some(f.a), None, Some(f.c)),
+        (None, Some(f.b), Some(f.c)),
+        (Some(f.a), Some(f.b), Some(f.c))))
+    }
   }
 
-  property("Cuber works for Bar") = forAll { b: Bar =>
-    Cuber.cuber(b) == List(
-      (None, None),
-      (Some(b.a), None),
-      (None, Some(b.foo)),
-      (Some(b.a), Some(b.foo)))
+  property("Cuber works for Bar") {
+    forAll { b: Bar =>
+      assert(Cuber.cuber(b) == List(
+        (None, None),
+        (Some(b.a), None),
+        (None, Some(b.foo)),
+        (Some(b.a), Some(b.foo))))
+    }
   }
 
-  property("Cuber works for Baz") = forAll { b: Baz =>
-    Cuber.cuber(b) == List(
-      Tuple1(None),
-      Tuple1(Some(b.a)))
+  property("Cuber works for Baz") {
+    forAll { b: Baz =>
+      assert(Cuber.cuber(b) == List(
+        Tuple1(None),
+        Tuple1(Some(b.a))))
+    }
   }
 
-  property("Roller works for Foo") = forAll { f: Foo =>
-    Roller.roller(f) == List(
-      (None, None, None),
-      (Some(f.a), None, None),
-      (Some(f.a), Some(f.b), None),
-      (Some(f.a), Some(f.b), Some(f.c)))
+  property("Roller works for Foo") {
+    forAll { f: Foo =>
+      assert(Roller.roller(f) == List(
+        (None, None, None),
+        (Some(f.a), None, None),
+        (Some(f.a), Some(f.b), None),
+        (Some(f.a), Some(f.b), Some(f.c))))
+    }
   }
 
-  property("Roller works for Bar") = forAll { b: Bar =>
-    Roller.roller(b) == List(
-      (None, None),
-      (Some(b.a), None),
-      (Some(b.a), Some(b.foo)))
+  property("Roller works for Bar") {
+    forAll { b: Bar =>
+      assert(Roller.roller(b) == List(
+        (None, None),
+        (Some(b.a), None),
+        (Some(b.a), Some(b.foo))))
+    }
   }
 
-  property("Roller works for Baz") = forAll { b: Baz =>
-    Roller.roller(b) == List(
-      Tuple1(None),
-      Tuple1(Some(b.a)))
+  property("Roller works for Baz") {
+    forAll { b: Baz =>
+      assert(Roller.roller(b) == List(
+        Tuple1(None),
+        Tuple1(Some(b.a))))
+    }
   }
 }
