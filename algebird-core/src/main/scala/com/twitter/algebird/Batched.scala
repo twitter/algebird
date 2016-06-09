923273a9cf6a06a10985eec414f418175cca1dae
Oscar Boykin <oscar@stripe.com>
2016-06-08 19:32:50 -1000

    Add some more methods to Batched for use with Fold/Aggregator

--- algebird-core/src/main/scala/com/twitter/algebird/Batched.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Batched.scala
@@ -20,6 +20,30 @@ object Batched {
   def apply[T](t: T): Batched[T] =
     Item(t)
 
+  def aggregator[A, B, C](batchSize: Int, agg: Aggregator[A, B, C]): Aggregator[A, Batched[B], C] = new Aggregator[A, Batched[B], C] {
+    def prepare(a: A): Batched[B] = Item(agg.prepare(a))
+    def semigroup: Semigroup[Batched[B]] = new BatchedSemigroup(batchSize, agg.semigroup)
+    def present(b: Batched[B]): C = agg.present(b.sum(agg.semigroup))
+  }
+
+  def monoidAggregator[A, B, C](batchSize: Int, agg: MonoidAggregator[A, B, C]): MonoidAggregator[A, Batched[B], C] =
+    new MonoidAggregator[A, Batched[B], C] {
+      def prepare(a: A): Batched[B] = Item(agg.prepare(a))
+      def monoid: Monoid[Batched[B]] = new BatchedMonoid(batchSize, agg.monoid)
+      def present(b: Batched[B]): C = agg.present(b.sum(agg.semigroup))
+    }
+
+  def foldOption[T: Semigroup](batchSize: Int): Fold[T, Option[T]] =
+    Fold.foldLeft[T, Option[Batched[T]]](None: Option[Batched[T]]) {
+      case (Some(b), t) => Some(b.combine(Item(t)))
+      case (None, t) => Some(Item(t))
+    }
+      .map(_.map(_.sum))
+
+  def fold[T: Monoid](batchSize: Int): Fold[T, T] =
+    Fold.foldLeft[T, Batched[T]](Batched(Monoid.zero[T])) { (b, t) => b.combine(Item(t)) }
+      .map(_.sum)
+
   /**
    * This represents a single (unbatched) value.
    */
@@ -87,5 +111,16 @@ class BatchedSemigroup[T](batchSize: Int, sg: Semigroup[T]) extends Semigroup[Ba
     val next = Batched.Items(a, b)
     if (next.size < batchSize) next
     else Batched.Item(next.sum(sg))
+  }
+}
+
+class BatchedMonoid[T](batchSize: Int, monoid: Monoid[T]) extends BatchedSemigroup(batchSize, monoid) with Monoid[Batched[T]] {
+  def zero: Batched[T] = Batched(monoid.zero)
+  override def isNonZero(b: Batched[T]): Boolean = isNonZeroRec(List(b))
+
+  @annotation.tailrec private def isNonZeroRec(stack: List[Batched[T]]): Boolean = stack match {
+    case Nil => true
+    case Batched.Item(i) :: tail => monoid.isNonZero(i) && isNonZeroRec(tail)
+    case Batched.Items(a, b) :: tail => isNonZeroRec(a :: b :: tail)
   }
 }
