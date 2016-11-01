48fbcc92dbe65523bea36fc4ebd6810568aa4af2
Sam Ritchie <sritchie@stripe.com>
2016-11-01 08:34:48 -0600

    convert to buckets with addAll

--- algebird-core/src/main/scala/com/twitter/algebird/ExpHist.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/ExpHist.scala
@@ -8,129 +8,140 @@ import scala.annotation.tailrec
  * http://ilpubs.stanford.edu:8090/504/1/2001-34.pdf.
  */
 object ExpHist {
-  import Canonical.Exp2
+  case class Bucket(size: Long, timestamp: Long)
 
-  // Same as math.ceil(x / 2.0).toInt
-  @inline def div2Ceil(x: Int): Int =
-    (x >>> 1) + (if ((x & 1) == 0) 0 else case)
+  object Bucket {
+    implicit val ord: Ordering[Bucket] = Ordering.by { b: Bucket => (b.timestamp, b.size) }
+  }
 
   case class Config(epsilon: Double, windowSize: Long) {
-    val l: Int = div2Ceil(math.ceil(1 / epsilon).toInt)
+    val k: Int = math.ceil(1 / epsilon).toInt
+    val l: Int = math.ceil(k / 2.0).toInt
 
     // Returns the last timestamp before the window. any ts <= [the
     // returned timestamp] is outside the window.
     def expiration(currTime: Long): Long = currTime - windowSize
-  }
-
-  def empty(conf: Config): ExpHist = ExpHist(conf, Vector.empty, Vector.empty, 0L)
-
-  def from(i: Long, ts: Long, conf: Config): ExpHist = {
-    val sizes = Canonical.fromLong(i, conf.l)
-    ExpHist(conf, Vector.fill(sizes.sum)(ts), sizes, ts)
+    def dropExpired(buckets: Vector[Bucket], currTime: Long): (Long, Vector[Bucket]) =
+      ExpHist.dropExpired(buckets, expiration(currTime))
   }
 
   /**
-   * Takes a vector of timestamps sorted in descending order (newest
-   * first) and a cutoff and returns a vector containing only
-   * timestamps AFTER the cutoff.
+   * Create an empty instance with the supplied Config.
    */
-  def dropExpired(timestamps: Vector[Long], cutoff: Long): Vector[Long] =
-    timestamps.reverse.dropWhile(_ <= cutoff).reverse
+  def empty(conf: Config): ExpHist = ExpHist(conf, Vector.empty, 0L, 0L)
+
+  /**
+   *  Create an instance from a number.
+   */
+  def from(i: Long, ts: Long, conf: Config): ExpHist = {
+    val buckets = Canonical.longToBuckets(i, conf.l).map(Bucket(_, ts))
+    ExpHist(conf, buckets, i, ts)
+  }
+
+  /**
+   * Takes a vector of buckets sorted in descending order (newest
+   * first) and a cutoff and returns
+   *
+   * - The sum of the sizes of the dropped buckets
+   * - a vector containing only buckets with timestamps AFTER the cutoff.
+   */
+  def dropExpired(timestamps: Vector[Bucket], cutoff: Long): (Long, Vector[Bucket]) = {
+    val (dropped, remaining) = timestamps.reverse.span(_.timestamp <= cutoff)
+    (dropped.map(_.size).sum, remaining.reverse)
+  }
 
   /**
    * Rebuckets the vector of inputs into buckets of the supplied
    * sequence of sizes. Returns only the associated `T` information
    * (timestamp, for example).
    */
-  def rebucket[T](v: Vector[(Long, T)], buckets: Vector[Exp2]): Vector[T] = {
-    if (buckets.isEmpty) Vector.empty
+  def rebucket(buckets: Vector[Bucket], desired: Vector[Long]): Vector[Bucket] =
+    if (desired.isEmpty) Vector.empty
     else {
-      val exp2 +: tail = buckets
-      val remaining = Canonical.drop(exp2.value, v)
-      v.head._2 +: rebucket(remaining, tail)
+      val bucketSize +: tail = desired
+      val remaining = drop(bucketSize, buckets)
+      buckets.head.copy(size = bucketSize) +: rebucket(remaining, tail)
+    }
+
+  /**
+   * @param toDrop total count to remove from the left of `input`.
+   * @param input buckets
+   * @return Vector with buckets, or pieces of buckets, with sizes
+   *         totalling `toDrop` items removed from the head. If an
+   *         element wasn't fully consumed, the remainder will be
+   *         stuck back onto the head.
+   */
+  @tailrec private[this] def drop(toDrop: Long, input: Vector[Bucket]): Vector[Bucket] = {
+    val (b @ Bucket(count, _)) +: tail = input
+    (toDrop - count) match {
+      case 0 => tail
+      case x if x < 0 => b.copy(size = -x) +: tail
+      case x if x > 0 => drop(x, tail)
     }
   }
 }
 
-// TODO: Interesting that relative error only depends on sizes, not on
-// timestamps. You could totally get a relative error off of some
-// number without ever going through this business, then use that to
-// decide when to evict stuff from the bucket.
-case class ExpHist(conf: ExpHist.Config, timestamps: Vector[Long], sizes: Vector[Int], time: Long) {
+case class ExpHist(conf: ExpHist.Config, buckets: Vector[ExpHist.Bucket], total: Long, time: Long) {
+  import ExpHist.Bucket
+
   def step(newTime: Long): ExpHist =
     if (newTime <= time) this
     else {
-      val filtered = ExpHist.dropExpired(timestamps, conf.expiration(newTime))
-      val bucketsDropped = timestamps.size - filtered.size
-      copy(
-        time = newTime,
-        timestamps = filtered,
-        sizes = Canonical.dropBiggest(bucketsDropped, sizes.reverse).reverse)
+      val (dropped, filtered) = conf.dropExpired(buckets, newTime)
+      copy(time = newTime, buckets = filtered, total = total - dropped)
     }
-
-  // Efficient implementation of add. To make this solid we'll want to
-  // keep a buffer of new items and only add when the buffer expires.
-  def add(i: Long, timestamp: Long): ExpHist = {
-    val self = step(timestamp)
-    if (i == 0) self
-    else {
-      // calculate canonical representation of the NEW total.
-      val newSizes = Canonical.fromLong(self.total + i, conf.l)
-
-      // Vector[TotalSizeOfEachBucket]
-      // Vector(3,3,2) expands out to
-      // Vector((1, ts),(1, ts),.... 1,2,2,2,4,4) == bucketSizes
-      val bucketSizes = Canonical.toBuckets(self.sizes).map(_.value)
-
-      // this is how we'd implement addAll:
-      val inputs = (i, timestamp) +: (bucketSizes zip self.timestamps)
-      self.copy(
-        timestamps = ExpHist.rebucket(inputs, Canonical.toBuckets(newSizes)),
-        sizes = newSizes)
-    }
-  }
 
   def inc(timestamp: Long): ExpHist = add(1L, timestamp)
 
-  def last: Long = if (sizes.isEmpty) 0L else (1 << (sizes.size - 1))
+  def add(delta: Long, timestamp: Long): ExpHist = {
+    val self = step(timestamp)
+    if (delta == 0) self
+    else self.addAllWithoutStep(Vector(Bucket(delta, timestamp)), delta)
+  }
 
-  def total: Long = Canonical.expand(sizes)
+  def addAll(unsorted: Vector[Bucket]): ExpHist =
+    if (unsorted.isEmpty) this
+    else {
+      val sorted = unsorted.sorted(Ordering[Bucket].reverse)
+      val delta = sorted.map(_.size).sum
+      val timestamp = sorted.head.timestamp
+      if (delta == 0)
+        step(timestamp)
+      else
+        addAllWithoutStep(sorted, delta).step(timestamp)
+    }
 
-  def lowerBoundSum: Long = total - last
+  // Note that this internal method assumes that the instance is
+  // stepped forward already, and does NOT try to step internally. It
+  // also assumes that `items` is sorted in ASCENDING order, with
+  // newer items on the right side.
+  // private[ExpHist]
+  def addAllWithoutStep(items: Vector[Bucket], delta: Long): ExpHist = {
+    val inputs = items ++ buckets
+    val desiredBuckets = Canonical.longToBuckets(total + delta, conf.l)
+    copy(
+      buckets = ExpHist.rebucket(inputs, desiredBuckets),
+      total = total + delta)
+  }
+
+  def oldestBucketSize: Long = if (buckets.isEmpty) 0L else buckets.last.size
+
+  def lowerBoundSum: Long = total - oldestBucketSize
 
   def upperBoundSum: Long = total
 
-  def guess: Double = total - (last - 1) / 2.0
+  def guess: Double = total - (oldestBucketSize - 1) / 2.0
 
   def relativeError: Double =
     if (total == 0) 0.0
     else {
-      val minInsideWindow = total + 1 - last
-      val absoluteError = (last - 1) / 2.0
+      val minInsideWindow = total + 1 - oldestBucketSize
+      val absoluteError = (oldestBucketSize - 1) / 2.0
       absoluteError / minInsideWindow
     }
-
-  /**
-   * Returns the same ExpHist with a new window. If the new window is
-   * smaller than the current window, evicts older items.
-   */
-  def withWindow(newWindow: Long): ExpHist =
-    copy(conf = conf.copy(windowSize = newWindow)).step(time)
-
-  // Returns the vector of bucket sizes from largest to smallest.
-  def windows: Vector[Long] =
-    for {
-      (numBuckets, idx) <- sizes.zipWithIndex.reverse
-      bucket <- List.fill(numBuckets)(1L << idx)
-    } yield bucket
 }
 
 object Canonical {
-  case class Exp2(exp: Int) extends AnyVal { l =>
-    def double: Exp2 = Exp2(exp + 1)
-    def value: Long = 1 << exp
-  }
-
   @inline private[this] def floorPowerOfTwo(x: Long): Int =
     JLong.numberOfTrailingZeros(JLong.highestOneBit(x))
 
@@ -146,35 +157,10 @@ object Canonical {
    * where `i` is the vector index, used to represent s for a given
    * k.
    *
-   * `s` is the "sum of the sizes of all of the buckets tracked by
+   * `s` is the sum of the sizes of all of the buckets tracked by
    * the ExpHist instance.
    *
-   * `l` is... well, gotta explain that based on the paper.
-   *
-   * 1
-   * 11
-   * 111
-   * 112
-   * 1112
-   * 1122
-   *
-   * 1
-   * 2
-   * 3
-   * 2 1
-   * 3 1
-   * 2 2
-   * 3 2
-   * 2 3
-   * 3 3
-   * 2 2 1
-   * 3 2 1
-   * 2 3 1
-   * 3 3 1
-   * 2 2 2
-   * 3 2 2
-   * 2 3 2
-   * 3 3 2
+   * `l` or `l + 1` buckets allowed per size.
    */
   def fromLong(s: Long, l: Int): Vector[Int] = {
     if (s <= 0) Vector.empty
@@ -187,23 +173,10 @@ object Canonical {
     }
   }
 
-  def lastBucketSize(s: Long, l: Int): Int = {
-    val num = s + l
-    val denom = l + 1
-    val j = floorPowerOfTwo(num / denom)
-    val offset = (num - (denom << j)).toInt
-    (quotient(offset, j) + 1)
-  }
-
-  /**
-   * Total number of buckets required to represent this number.
-   */
-  def bucketsRequired(s: Long, l: Short): Int = fromLong(s, l).sum
-
   /**
    * Expand out a number's l-canonical form into the original number.
    */
-  def expand(rep: Vector[Int]): Long =
+  def toLong(rep: Vector[Int]): Long =
     if (rep.isEmpty) 0L
     else {
       rep.iterator.zipWithIndex
@@ -211,48 +184,16 @@ object Canonical {
         .reduce(_ + _)
     }
 
-  // Expands out the compressed form to a list of the bucket sizes (sized with exponent only)
-  //
-  // TODO: law - toBuckets(rep).size == rep.sum
-  def toBuckets(rep: Vector[Int]): Vector[Exp2] =
-    rep.zipWithIndex.flatMap { case (i, exp) => List.fill(i)(Exp2(exp)) }
+  /**
+   * Expands out the compressed form to a list of the bucket sizes in
+   * ascending order.
+   */
+  def toBuckets(rep: Vector[Int]): Vector[Long] =
+    rep.zipWithIndex.flatMap { case (i, exp) => List.fill(i)(1L << exp) }
 
   /**
-   * Given a canonical representation, takes a number of buckets to
-   * drop (starting with the largest buckets!) and removes that
-   * number of buckets from the end.
-   *
-   * (The sum of `canonical` is the total number of buckets in the
-   * representation.)
+   * Returns a sequence of (ascending-order) bucket sizes required to
+   * store the supplied `s` in l-canonical form.
    */
-  @tailrec def dropBiggest(bucketsToDrop: Int, canonical: Vector[Int]): Vector[Int] =
-    (bucketsToDrop, canonical) match {
-      case (0, l) => l
-      case (toDrop, count +: tail) =>
-        (toDrop - count) match {
-          case 0 => tail
-          case x if x < 0 => -x +: tail
-          case x if x > 0 => dropBiggest(x, tail)
-        }
-      case _ => Vector.empty[Int]
-    }
-
-  /**
-   * @param x total count to remove from the head of `input`.
-   * @param input pairs of (count, T).
-   * @return Vector with x total items removed from the head; if
-   *         an element wasn't fully consumed, the remainder will be
-   *         stuck back onto the head.
-   */
-  @tailrec def drop[T](x: Long, input: Vector[(Long, T)]): Vector[(Long, T)] =
-    (x, input) match {
-      case (0, input) => input
-      case (toDrop, (count, t) +: tail) =>
-        (toDrop - count) match {
-          case 0 => tail
-          case x if x < 0 => (-x, t) +: tail
-          case x if x > 0 => drop(x, tail)
-        }
-      case _ => Vector.empty[(Long, T)]
-    }
+  def longToBuckets(s: Long, l: Int): Vector[Long] = toBuckets(fromLong(s, l))
 }
--- algebird-test/src/test/scala/com/twitter/algebird/ExpHistLaws.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/ExpHistLaws.scala
@@ -13,91 +13,81 @@ object PosNum {
     Arbitrary(for { p <- Gen.posNum[T] } yield PosNum(p))
 }
 
-case class Tick(count: Long, timestamp: Long)
-
-object Tick {
-  implicit val ord: Ordering[Tick] =
-    Ordering.by { t: Tick => (t.timestamp, t.count) }
-
-  implicit val arb: Arbitrary[Tick] =
-    Arbitrary(for {
-      ts <- Gen.posNum[Long]
-      tick <- Gen.posNum[Long]
-    } yield Tick(tick - 1L, ts))
+case class NonEmptyVector[T](items: Vector[T]) {
+  def sorted(implicit ev: Ordering[T]): Vector[T] = items.sorted
 }
 
-case class NonEmptyList[T](items: List[T]) {
-  def sorted(implicit ev: Ordering[T]): List[T] = items.sorted
-}
-
-object NonEmptyList {
-  implicit def arb[T: Ordering: Arbitrary]: Arbitrary[NonEmptyList[T]] =
+object NonEmptyVector {
+  implicit def arb[T: Ordering: Arbitrary]: Arbitrary[NonEmptyVector[T]] =
     Arbitrary(for {
       l <- Gen.nonEmptyListOf(arbitrary[T])
-    } yield NonEmptyList[T](l))
+    } yield NonEmptyVector[T](l.toVector))
 }
 
 class ExpHistLaws extends PropSpec with PropertyChecks with Matchers {
-  import BaseProperties._
+  import ExpHist.{ Bucket, Config }
 
-  implicit val conf: Arbitrary[ExpHist.Config] =
+  implicit val arb: Arbitrary[Bucket] =
+    Arbitrary(for {
+      count <- Gen.posNum[Long]
+      timestamp <- Gen.posNum[Long]
+    } yield Bucket(count - 1L, timestamp))
+
+  implicit val conf: Arbitrary[Config] =
     Arbitrary(for {
       k <- Gen.posNum[Short]
       windowSize <- Gen.posNum[Long]
-    } yield ExpHist.Config(1 / k.toDouble, windowSize))
-
-  def addAll(e: ExpHist, ticks: List[Tick]): ExpHist =
-    ticks.foldLeft(e) {
-      case (e, Tick(count, timestamp)) =>
-        e.add(count, timestamp)
-    }
+    } yield Config(1 / k.toDouble, windowSize))
 
   implicit val expHist: Arbitrary[ExpHist] =
     Arbitrary(for {
-      ticks <- arbitrary[List[Tick]]
+      buckets <- arbitrary[Vector[Bucket]]
       conf <- arbitrary[ExpHist.Config]
-    } yield addAll(ExpHist.empty(conf), ticks))
+    } yield ExpHist.empty(conf).addAll(buckets))
 
   // The example in the paper is actually busted, based on his
   // algorithm. He says to assume that k/2 == 2, but then he promotes
   // as if k/2 == 1, ie k == 2.
   property("example from paper") {
+    // Returns the vector of bucket sizes from largest to smallest.
+    def windows(e: ExpHist): Vector[Long] = e.buckets.reverse.map(_.size)
+
     val e = ExpHist.empty(ExpHist.Config(0.5, 100))
     val plus76 = e.add(76, 0)
 
     val inc = plus76.inc(0)
     val incinc = inc.add(2, 0)
 
-    plus76.windows shouldBe Vector(32, 16, 8, 8, 4, 4, 2, 1, 1)
-    inc.windows shouldBe Vector(32, 16, 8, 8, 4, 4, 2, 2, 1)
-    incinc.windows shouldBe Vector(32, 16, 16, 8, 4, 2, 1)
+    assert(windows(plus76) == Vector(32, 16, 8, 8, 4, 4, 2, 1, 1))
+    assert(windows(inc) == Vector(32, 16, 8, 8, 4, 4, 2, 2, 1))
+    assert(windows(incinc) == Vector(32, 16, 16, 8, 4, 2, 1))
   }
 
   property("adding i results in upperBoundSum == i") {
-    forAll { (conf: ExpHist.Config, tick: Tick) =>
+    forAll { (conf: ExpHist.Config, bucket: Bucket) =>
       assert(ExpHist.empty(conf)
-        .add(tick.count, tick.timestamp)
-        .upperBoundSum == tick.count)
+        .add(bucket.size, bucket.timestamp)
+        .upperBoundSum == bucket.size)
     }
   }
 
   property("empty.add and from are identical") {
-    forAll { (conf: ExpHist.Config, tick: Tick) =>
-      val byAdd = ExpHist.empty(conf).add(tick.count, tick.timestamp)
-      val byFrom = ExpHist.from(tick.count, tick.timestamp, conf)
+    forAll { (conf: ExpHist.Config, bucket: Bucket) =>
+      val byAdd = ExpHist.empty(conf).add(bucket.size, bucket.timestamp)
+      val byFrom = ExpHist.from(bucket.size, bucket.timestamp, conf)
       assert(byAdd == byFrom)
     }
   }
 
   property("step should be idempotent") {
-    forAll { (expHist: ExpHist, tick: Tick) =>
-      val ts = tick.timestamp
+    forAll { (expHist: ExpHist, bucket: Bucket) =>
+      val ts = bucket.timestamp
       val stepped = expHist.step(ts)
       assert(stepped == stepped.step(ts))
 
-      val added = expHist.add(tick.count, ts)
+      val added = expHist.add(bucket.size, ts)
       val addThenStep = added.step(ts)
-      val stepThenAdd = stepped.add(tick.count, ts)
+      val stepThenAdd = stepped.add(bucket.size, ts)
 
       assert(added == addThenStep)
       assert(added == stepThenAdd)
@@ -111,59 +101,104 @@ class ExpHistLaws extends PropSpec with PropertyChecks with Matchers {
     }
   }
 
+  // property("add matches addAll") {
+  //   forAll { (unsorted: NonEmptyVector[Bucket], conf: ExpHist.Config) =>
+  //     val e = ExpHist.empty(conf)
+  //     val items = unsorted.sorted
+  //     val withAdd = items.foldLeft(e) {
+  //       case (e, Bucket(count, timestamp)) =>
+  //         e.add(count, timestamp)
+  //     }
+
+  //     assert(withAdd == e.addAll(items))
+  //   }
+  // }
+
   // This is the meat!
-  property("bounded error of the query") {
-    forAll { (items: NonEmptyList[Tick], conf: ExpHist.Config) =>
-      val ticks = items.sorted
+  def testBounds(eh: ExpHist, actualSum: Long) {
+    val upperBound = eh.upperBoundSum
+    val lowerBound = eh.lowerBoundSum
+    assert(lowerBound <= actualSum)
+    assert(upperBound >= actualSum)
 
-      val full = addAll(ExpHist.empty(conf), ticks)
+    val maxOutsideWindow = eh.oldestBucketSize - 1
+    val minInsideWindow = 1 + eh.total - eh.oldestBucketSize
+    val absoluteError = maxOutsideWindow / 2.0
+    val relativeError = absoluteError / minInsideWindow
 
-      val finalTs = ticks.last.timestamp
+    // local relative error calc:
+    assert(relativeError <= eh.conf.epsilon)
 
-      val actualSum = ticks.collect {
-        case Tick(count, ts) if ts > (finalTs - conf.windowSize) => count
-      }.sum
+    // Instance's relative error calc:
+    assert(eh.relativeError <= eh.conf.epsilon)
 
-      val upperBound = full.upperBoundSum
-      val lowerBound = full.lowerBoundSum
-      assert(lowerBound <= actualSum)
-      assert(upperBound >= actualSum)
+    // Error can never be above 0.5.
+    assert(eh.relativeError <= 0.5)
+  }
 
-      val maxOutsideWindow = full.last - 1
-      val minInsideWindow = 1 + full.total - full.last
-      val absoluteError = maxOutsideWindow / 2.0
-      val relativeError = absoluteError / minInsideWindow
+  def actualBucketSum(buckets: Vector[Bucket], exclusiveCutoff: Long): Long =
+    buckets.collect {
+      case Bucket(count, ts) if ts > exclusiveCutoff => count
+    }.sum
 
-      // local relative error calc:
-      assert(relativeError <= conf.epsilon)
+  property("bounded error of the query with addAll") {
+    forAll { (v: NonEmptyVector[Bucket], conf: Config) =>
+      val buckets = v.items
+      val recentTs = buckets.maxBy(_.timestamp).timestamp
+      val cutoff = conf.expiration(recentTs)
 
-      // Instance's relative error calc:
-      assert(full.relativeError <= conf.epsilon)
+      testBounds(
+        ExpHist.empty(conf).addAll(buckets),
+        actualBucketSum(buckets, cutoff))
+    }
+  }
 
-      // Error can never be above 0.5.
-      assert(full.relativeError <= 0.5)
+  property("bounded error of the query with add") {
+    forAll { (items: NonEmptyVector[Bucket], conf: Config) =>
+      val buckets = items.sorted
+      val recentTs = buckets.last.timestamp
+      val cutoff = conf.expiration(recentTs)
+
+      val full = buckets.foldLeft(ExpHist.empty(conf)) {
+        case (e, Bucket(c, t)) => e.add(c, t)
+      }
+
+      testBounds(full, actualBucketSum(buckets, cutoff))
     }
   }
 
   property("add and inc should generate the same results") {
-    forAll { (tick: Tick, conf: ExpHist.Config) =>
+    forAll { (bucket: Bucket, conf: ExpHist.Config) =>
       val e = ExpHist.empty(conf)
 
-      val incs = (0L until tick.count).foldLeft(e) {
-        case (acc, _) => acc.inc(tick.timestamp)
+      val incs = (0L until bucket.size).foldLeft(e) {
+        case (acc, _) => acc.inc(bucket.timestamp)
       }
 
-      val adds = e.add(tick.count, tick.timestamp)
+      val adds = e.add(bucket.size, bucket.timestamp)
 
       assert(incs.total == adds.total)
       assert(incs.lowerBoundSum == adds.lowerBoundSum)
       assert(incs.upperBoundSum == adds.upperBoundSum)
     }
   }
+}
 
+class CanonicalLaws extends PropSpec with PropertyChecks with Matchers {
   property("l-canonical representation round-trips") {
+    forAll { (i: PosNum[Long], l: PosNum[Short]) =>
+      assert(Canonical.toLong(Canonical.fromLong(i.value, l.value)) == i.value)
+    }
+  }
+
+  property("Sum of canonical rep is # of buckets required.") {
     forAll { (i: PosNum[Long], k: PosNum[Short]) =>
-      assert(Canonical.expand(Canonical.fromLong(i.value, k.value)) == i.value)
+      val rep = Canonical.fromLong(i.value, k.value)
+      val numBuckets = Canonical.toBuckets(rep).size
+      assert(numBuckets == rep.sum)
+
+      // longToBuckets works.
+      assert(Canonical.longToBuckets(i.value, k.value).size == numBuckets)
     }
   }
 
@@ -175,12 +210,6 @@ class ExpHistLaws extends PropSpec with PropertyChecks with Matchers {
         Canonical.fromLong(i.value, k.value).init.forall { numBuckets =>
           lower <= numBuckets && numBuckets <= upper
         })
-    }
-  }
-
-  property("impl of `div2Ceil` matches the simpler impl") {
-    forAll { i: Int =>
-      assert(ExpHist.div2Ceil(i) == math.ceil(i / 2.0).toInt)
     }
   }
 }
