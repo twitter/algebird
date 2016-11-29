282ca7e4d974fbc2caebd9f3c993601420d723e4
Sam Ritchie <sritchie@stripe.com>
2016-11-29 09:30:19 -0700

    increase tests and coverage for generated tuple aggregators

---
+++ algebird-test/src/test/scala/com/twitter/algebird/GeneratedAbstractAlgebraLaws.scala
@@ -0,0 +1,69 @@
+package com.twitter.algebird
+
+import com.twitter.algebird.BaseProperties._
+
+class GeneratedAbstractAlgebraLaws extends CheckProperties {
+  property("tuple2 is a ring") {
+    ringLaws[(Int, Int)]
+  }
+  property("tuple3 is a ring") {
+    ringLaws[(Int, Int, Int)]
+  }
+  property("tuple4 is a ring") {
+    ringLaws[(Int, Int, Int, Int)]
+  }
+  property("tuple5 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int)]
+  }
+  property("tuple6 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple7 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple8 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple9 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple10 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple11 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple12 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple13 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple14 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple15 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple16 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple17 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple18 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple19 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple20 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple21 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+  property("tuple22 is a ring") {
+    ringLaws[(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)]
+  }
+}
---
+++ algebird-test/src/test/scala/com/twitter/algebird/GeneratedProductAlgebraLaws.scala
@@ -0,0 +1,111 @@
+package com.twitter.algebird
+
+import com.twitter.algebird.BaseProperties._
+
+class GeneratedProductAlgebraLaws extends CheckProperties {
+  property("Product2Ring is a ring") {
+    type T = (Int, Int)
+    implicit val ring = Ring[T, Int, Int](Tuple2.apply, Tuple2.unapply)
+    ringLaws[T]
+  }
+  property("Product3Ring is a ring") {
+    type T = (Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int](Tuple3.apply, Tuple3.unapply)
+    ringLaws[T]
+  }
+  property("Product4Ring is a ring") {
+    type T = (Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int](Tuple4.apply, Tuple4.unapply)
+    ringLaws[T]
+  }
+  property("Product5Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int](Tuple5.apply, Tuple5.unapply)
+    ringLaws[T]
+  }
+  property("Product6Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int](Tuple6.apply, Tuple6.unapply)
+    ringLaws[T]
+  }
+  property("Product7Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int](Tuple7.apply, Tuple7.unapply)
+    ringLaws[T]
+  }
+  property("Product8Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int](Tuple8.apply, Tuple8.unapply)
+    ringLaws[T]
+  }
+  property("Product9Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple9.apply, Tuple9.unapply)
+    ringLaws[T]
+  }
+  property("Product10Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple10.apply, Tuple10.unapply)
+    ringLaws[T]
+  }
+  property("Product11Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple11.apply, Tuple11.unapply)
+    ringLaws[T]
+  }
+  property("Product12Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple12.apply, Tuple12.unapply)
+    ringLaws[T]
+  }
+  property("Product13Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple13.apply, Tuple13.unapply)
+    ringLaws[T]
+  }
+  property("Product14Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple14.apply, Tuple14.unapply)
+    ringLaws[T]
+  }
+  property("Product15Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple15.apply, Tuple15.unapply)
+    ringLaws[T]
+  }
+  property("Product16Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple16.apply, Tuple16.unapply)
+    ringLaws[T]
+  }
+  property("Product17Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple17.apply, Tuple17.unapply)
+    ringLaws[T]
+  }
+  property("Product18Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple18.apply, Tuple18.unapply)
+    ringLaws[T]
+  }
+  property("Product19Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple19.apply, Tuple19.unapply)
+    ringLaws[T]
+  }
+  property("Product20Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple20.apply, Tuple20.unapply)
+    ringLaws[T]
+  }
+  property("Product21Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple21.apply, Tuple21.unapply)
+    ringLaws[T]
+  }
+  property("Product22Ring is a ring") {
+    type T = (Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int)
+    implicit val ring = Ring[T, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int](Tuple22.apply, Tuple22.unapply)
+    ringLaws[T]
+  }
+}
