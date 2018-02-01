afd2f07792c364e516817f72cba8ec80fed23a1f
Christian Griset <cdg@stripe.com>
2018-01-31 21:52:01 -0800

    Extend tests

--- algebird-test/src/test/scala/com/twitter/algebird/WindowLawsTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/WindowLawsTest.scala
@@ -12,7 +12,37 @@ class WindowLaws extends CheckProperties {
     forAll { (ts0: List[Int], n: Int) =>
       val ts = ts0.takeRight(n)
       val mon = new WindowMonoid(n)
-      assert(mon.sum(ts0.map( Window(_) )).total == ts.sum)
+      assert(mon.sum(ts0.map(Window(_))).total == ts.sum)
+    }
+  }
+
+  property("We correctly create a window from traversable") {
+    forAll { (ts0: List[Int], n: Int) =>
+      val mon = new WindowMonoid(n)
+      val right = Queue(ts0.takeRight(n): _*)
+      val expected = Window(right.sum, right)
+      val got = mon.fromTraversable(ts0)
+      assert(got == expected)
+    }
+  }
+
+  property("We correctly combine windows") {
+    forAll { (left: List[Int], right: List[Int], n: Int) =>
+      val mon = new WindowMonoid(n)
+      val trunc = Queue((left ::: right).takeRight(n): _*)
+      val expected = Window(trunc.sum, trunc)
+      val got = mon.sum(mon.fromTraversable(left), mon.fromTraversable(right))
+      assert(expected == got)
+    }
+  }
+
+  property("We correctly overrode sumOption") {
+    forAll { (ts0: List[Int], n: Int) =>
+      val mon = new WindowMonoid(n)
+      val got = mon.sumOption(ts0.map { Window(_) })
+      val trunc = Queue(ts0.takeRight(n): _*)
+      val expected = if (n == 0) None else Some(Window(trunc.sum, trunc))
+      assert(expected == got)
     }
   }
 
