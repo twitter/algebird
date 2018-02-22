9ea7772861617b777fcf0ba72968b9ae5cdfdabd
P. Oscar Boykin <johnynek@users.noreply.github.com>
2018-02-22 07:42:23 -1000

    Optimize Window monoid a bit (#652)
    * Optimize Window monoid a bit
    
    * avoid takeRight

--- algebird-test/src/test/scala/com/twitter/algebird/WindowLawsTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/WindowLawsTest.scala
@@ -11,14 +11,20 @@ import org.scalacheck.Prop.forAll
 
 class WindowLaws extends CheckProperties {
 
-  implicit val mon = WindowMonoid[Int](5)
-  implicit val wGen = Arbitrary {
-    for (
-      v <- Gen.choose(-1000, 1000)
-    ) yield (Window[Int](v))
-  }
+  implicit val mon = Window.monoid[Int](5)
 
-  property("Window obeys monoid laws") { monoidLaws[Window[Int]] }
+  implicit def wGen[A: Arbitrary](implicit wm: WindowMonoid[A]): Arbitrary[Window[A]] =
+    Arbitrary {
+      for {
+        as <- Gen.listOf(Arbitrary.arbitrary[A])
+      } yield Window.fromIterable(as)
+    }
+
+  property("Window obeys monoid laws using a group") { monoidLaws[Window[Int]] }
+  property("Window obeys monoid laws using a monoid") {
+    implicit val mon = Window.monoid[String](5)
+    monoidLaws[Window[String]]
+  }
 }
 
 class WindowTest extends CheckProperties {
@@ -26,7 +32,7 @@ class WindowTest extends CheckProperties {
     forAll { (ts0: List[Int], pn: PosNum[Int]) =>
       val n = pn.value
       val ts = ts0.takeRight(n)
-      val mon = WindowMonoid[Int](n)
+      val mon = Window.monoid[Int](n)
       mon.sum(ts0.map(Window(_))).total == ts.sum
     }
   }
@@ -34,7 +40,7 @@ class WindowTest extends CheckProperties {
   property("We correctly create a window from iterable") {
     forAll { (ts0: List[Int], pn: PosNum[Int]) =>
       val n = pn.value
-      val mon = WindowMonoid[Int](n)
+      val mon = Window.monoid[Int](n)
       val right = Queue(ts0.takeRight(n): _*)
       val expected = Window(right.sum, right)
       val got = mon.fromIterable(ts0)
@@ -45,7 +51,7 @@ class WindowTest extends CheckProperties {
   property("We correctly combine windows") {
     forAll { (left: List[Int], right: List[Int], pn: PosNum[Int]) =>
       val n = pn.value
-      val mon = WindowMonoid[Int](n)
+      val mon = Window.monoid[Int](n)
       val trunc = Queue((left ::: right).takeRight(n): _*)
       val expected = Window(trunc.sum, trunc)
       val got = mon.plus(mon.fromIterable(left), mon.fromIterable(right))
@@ -56,7 +62,7 @@ class WindowTest extends CheckProperties {
   property("We correctly overrode sumOption") {
     forAll { (ts0: List[Int], pn: PosNum[Int]) =>
       val n = pn.value
-      val mon = WindowMonoid[Int](n)
+      val mon = Window.monoid[Int](n)
       val got = mon.sumOption(ts0.map { Window(_) })
       val trunc = Queue(ts0.takeRight(n): _*)
       val expected = if (ts0.size == 0) None else Some(Window(trunc.sum, trunc))
