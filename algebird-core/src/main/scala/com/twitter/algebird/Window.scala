c0af713931e0cf27ecd5c09a7967ff0ae200eb81
Oscar Boykin <oscar.boykin@gmail.com>
2018-02-21 16:27:29 -1000

    Optimize Window monoid a bit

--- algebird-core/src/main/scala/com/twitter/algebird/Window.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Window.scala
@@ -17,6 +17,7 @@ limitations under the License.
 package com.twitter.algebird
 
 import scala.collection.immutable.Queue
+import java.io.Serializable
 import Operators._
 
 /**
@@ -41,7 +42,7 @@ import Operators._
  *
  *    implicit def w28Monoid[T](implicit p: Priority[Group[T], Monoid[T]]): Monoid[W28[T]] =
  *      new Monoid[W28[T]] {
- *        private val WT: Monoid[Window[T]] = WindowMonoid[T](windowSize)
+ *        private val WT: Monoid[Window[T]] = Window.monoid[T](windowSize)
  *        def zero = W28[T](WT.zero)
  *        def plus(a: W28[T], b: W28[T]): W28[T] =
  *          W28[T](WT.plus(a.window, b.window))
@@ -57,34 +58,36 @@ import Operators._
  */
 
 case class Window[T](total: T, items: Queue[T]) {
-  def size = this.items.size
+  def size: Int = items.size
 }
 
-object Window {
+object Window extends Serializable {
   def apply[T](v: T): Window[T] = Window[T](v, Queue[T](v))
-  def from[T](ts: Iterable[T])(implicit m: WindowMonoid[T]) = m.fromIterable(ts)
-}
+  def fromIterable[T](ts: Iterable[T])(implicit m: WindowMonoid[T]) = m.fromIterable(ts)
 
-/**
- * Provides a natural monoid for combining windows truncated to some window size.
- *
- * @param windowSize Upper limit of the number of items in a window.
- */
+  /**
+   * Build a monoid from either a group or a monoid
+   */
+  def monoid[T](size: Int)(implicit p: Priority[Group[T], Monoid[T]]): WindowMonoid[T] =
+    p match {
+      case Priority.Preferred(grp) => monoidFromGroup(size)(grp)
+      case Priority.Fallback(mon) => monoidFromMonoid(size)(mon)
+    }
 
+  /**
+   * This is the more efficient way to build a window
+   */
+  def monoidFromGroup[T: Group](size: Int): WindowMonoid[T] =
+    WindowMonoidFromGroup[T](size)
 
-case class WindowMonoid[T](
-    windowSize: Int
-)(implicit p: Priority[Group[T], Monoid[T]])
-    extends Monoid[Window[T]] {
+  def monoidFromMonoid[T: Monoid](size: Int): WindowMonoid[T] =
+    WindowMonoidFromMonoid[T](size)
 
-  require(windowSize >= 1, "Windows must have positive sizes")
-
-  def zero = p.fold(g => Window[T](g.zero, Queue.empty[T]))(m => Window(m.zero, Queue.empty[T]))
-
-  def plus(a: Window[T], b: Window[T]): Window[T] =
-    p.fold(g => plusG(a, b)(g))(m => plusM(a, b)(m))
-
-  def plusG(a: Window[T], b: Window[T])(implicit g: Group[T]): Window[T] =
+  /**
+   * This is a faster way to combine two Windows if you
+   * have a group
+   */
+  def combineWithGroup[T: Group](windowSize: Int, a: Window[T], b: Window[T]): Window[T] =
     if (b.items.size >= windowSize) {
       var total: T = b.total
       var q = b.items
@@ -107,18 +110,30 @@ case class WindowMonoid[T](
       Window(total, items)
     }
 
-  def plusM(a: Window[T], b: Window[T])(implicit m: Monoid[T]): Window[T] =
+  def combineWithMonoid[T: Monoid](windowSize: Int, a: Window[T], b: Window[T]): Window[T] =
     if (b.items.size >= windowSize) {
       val items = b.items.takeRight(windowSize)
-      val total = m.sum(items)
+      val total = Monoid.sum(items)
       Window(total, items)
     } else {
       // we need windowSize - b.items.size from `a`
       val fromA = a.items.takeRight(windowSize - b.items.size)
       val items = fromA ++ b.items
-      val total = m.sum(fromA) + b.total
+      val total = Monoid.sum(fromA) + b.total
       Window(total, items)
     }
+}
+
+/**
+ * Provides a natural monoid for combining windows truncated to some window size.
+ *
+ * @param windowSize Upper limit of the number of items in a window.
+ */
+abstract class WindowMonoid[T](windowSize: Int) extends Monoid[Window[T]] {
+  require(windowSize >= 1, s"Windows must have positive sizes, found $windowSize")
+
+  def monoid: Monoid[T]
+  val zero = Window(monoid.zero, Queue.empty)
 
   override def sumOption(ws: TraversableOnce[Window[T]]): Option[Window[T]] =
     if (ws.isEmpty) None
@@ -128,17 +143,35 @@ case class WindowMonoid[T](
       while (it.hasNext) {
         queue = (queue ++ it.next.items).takeRight(windowSize)
       }
-      val monT: Monoid[T] = p.join
-      Some(Window(monT.sum(queue), queue))
+      Some(Window(monoid.sum(queue), queue))
     }
 
   def fromIterable(ts: Iterable[T]): Window[T] = {
-    if(ts.size == 0) zero
+    if (ts.isEmpty) zero
     else {
-      val monT: Monoid[T] = p.join
-      val right = ts.toList.takeRight(windowSize)
-      val total = monT.sum(right)
-      Window(total, Queue(right: _*))
+      var queue = Queue.empty[T]
+      val it = ts.iterator
+      while (it.hasNext) {
+        // avoid materializing the whole list in memory
+        // at one time
+        queue = (queue :+ it.next).takeRight(windowSize)
+      }
+      val total = monoid.sum(queue)
+      Window(total, queue)
     }
   }
+}
+
+final case class WindowMonoidFromMonoid[T](windowSize: Int)(implicit m: Monoid[T]) extends WindowMonoid[T](windowSize) {
+  def monoid: Monoid[T] = m
+
+  override def plus(a: Window[T], b: Window[T]): Window[T] =
+    Window.combineWithMonoid(windowSize, a, b)
+}
+
+final case class WindowMonoidFromGroup[T](windowSize: Int)(implicit val group: Group[T]) extends WindowMonoid[T](windowSize) {
+  def monoid: Monoid[T] = group
+
+  def plus(a: Window[T], b: Window[T]): Window[T] =
+    Window.combineWithGroup(windowSize, a, b)
 }
