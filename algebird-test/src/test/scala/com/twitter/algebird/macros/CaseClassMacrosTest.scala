59ab0f60553d1e2ff8b52aacb8ae42ab4a379a4c
Oscar Boykin <oscar.boykin@gmail.com>
2018-05-11 15:24:50 -1000

    remove implicit from the caseclass macros

--- algebird-test/src/test/scala/com/twitter/algebird/macros/CaseClassMacrosTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/macros/CaseClassMacrosTest.scala
@@ -1,7 +1,6 @@
 package com.twitter.algebird.macros
 
 import com.twitter.algebird._
-import com.twitter.algebird.macros.caseclass._
 import com.twitter.algebird.macros.ArbitraryCaseClassMacro.arbitrary
 
 import org.scalacheck.{Arbitrary, Properties}
@@ -15,8 +14,19 @@ object CaseClassMacrosTest extends Properties("Case class macros") {
   implicit val arbitraryBaz: Arbitrary[Baz[Int]] = arbitrary[Baz[Int]]
 
   case class Foo(a: Int, b: Short, c: Long)
+  object Foo {
+    implicit val fooRing: Ring[Foo] = caseclass.ring
+  }
+
   case class Bar(a: Boolean, foo: Foo)
+  object Bar {
+    implicit val barRing: Ring[Bar] = caseclass.ring
+  }
+
   case class Baz[A](a: A, b: Short, c: A)
+  object Baz {
+    implicit def bazRing[A: Ring]: Ring[Baz[A]] = caseclass.ring
+  }
 
   property("Foo is a Semigroup") = semigroupLaws[Foo]
   property("Foo is a Monoid") = monoidLaws[Foo]
@@ -32,4 +42,14 @@ object CaseClassMacrosTest extends Properties("Case class macros") {
   property("Baz[Int] is a Monoid") = monoidLaws[Baz[Int]]
   property("Baz[Int] is a Group") = groupLaws[Baz[Int]]
   property("Baz[Int] is a Ring") = ringLaws[Baz[Int]]
+
+  //
+  // importing the case class macros does not break wrappers:
+  property("importing caseclass._ doesn't break Max") = forAll { (x: Int, y: Int) =>
+    Semigroup.plus(Max(x), Max(y)) == Max(x.max(y))
+  }
+
+  property("importing caseclass._ doesn't break Last") = forAll { (x: Int, y: Int) =>
+    Semigroup.plus(Last(x), Last(y)) == Last(y)
+  }
 }
