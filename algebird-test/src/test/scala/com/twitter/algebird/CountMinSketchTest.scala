93a0d3224e03ce46c2c7ab82afa9a054abaa2bea
Oscar Boykin <oscar.boykin@gmail.com>
2018-05-11 18:47:40 -1000

    somehow this constant is wrong on scala 2.12

--- algebird-test/src/test/scala/com/twitter/algebird/CountMinSketchTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/CountMinSketchTest.scala
@@ -967,7 +967,7 @@ class CMSFunctionsSpec extends PropSpec with PropertyChecks with Matchers {
   // For all i > 709, CMSFunctions.delta(i) will return 0.0, which is not the mathematically correct value but rather
   // the asymptote of the delta function.
   property("throw IAE when deriving delta from invalid depth values") {
-    val maxValidDelta = 709
+    val maxValidDelta = 745
     forAll(Gen.choose(maxValidDelta + 1, 10000)) { (invalidDepth: Int) =>
       // due to shrinking we can shrink to an invalid size, double check the size:
       // this is a known issue in scalacheck
