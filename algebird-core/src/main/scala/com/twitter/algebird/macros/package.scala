b823f2115a9669aa79cea9fc49b4ebaba69eafd0
P. Oscar Boykin <johnynek@users.noreply.github.com>
2018-05-11 18:31:11 -1000

    remove implicit from the caseclass macros (#657)
    * remove implicit from the caseclass macros
    
    * actually import caseclass._

--- algebird-core/src/main/scala/com/twitter/algebird/macros/package.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/macros/package.scala
@@ -12,7 +12,7 @@ package object macros {
 
     val isCaseClass = tpe.typeSymbol.isClass && tpe.typeSymbol.asClass.isCaseClass
     if (!isCaseClass)
-      c.abort(c.enclosingPosition, s"$T is not a case class")
+      c.abort(c.enclosingPosition, s"${tpe.typeSymbol} is not a case class")
   }
 
   private[macros] def getParams[T](c: Context)(
