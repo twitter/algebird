9f2df99c8812e083a78c5d03e823796f515a3fda
Oscar Boykin <oscar.boykin@gmail.com>
2018-05-14 15:39:23 -1000

    format test files

--- algebird-generic/src/test/scala/com/twitter/algebird/generic/AggregatorTest.scala
+++ algebird-generic/src/test/scala/com/twitter/algebird/generic/AggregatorTest.scala
@@ -43,10 +43,9 @@ abstract class ParallelAggregatorTest[
     with PropertyChecks {
 
   property(s"$name: parallel aggregator product (allOf)") {
-    forAll {
-      (a1: MonoidAggregator[A1, B1, C1], a2: MonoidAggregator[A2, B2, C2], axs: List[(A1, A2)]) =>
-        val a3 = ParallelAggregators.allOf(a1 :: a2 :: HNil)
-        a3(axs.map(_.productElements)) shouldBe (a1 zip a2)(axs).productElements
+    forAll { (a1: MonoidAggregator[A1, B1, C1], a2: MonoidAggregator[A2, B2, C2], axs: List[(A1, A2)]) =>
+      val a3 = ParallelAggregators.allOf(a1 :: a2 :: HNil)
+      a3(axs.map(_.productElements)) shouldBe a1.zip(a2)(axs).productElements
     }
   }
 
@@ -57,11 +56,9 @@ abstract class ParallelAggregatorTest[
         case Right(x) => Inr(Inl(x))
       }
     forAll {
-      (a1: MonoidAggregator[A1, B1, C1],
-       a2: MonoidAggregator[A2, B2, C2],
-       axs: List[Either[A1, A2]]) =>
+      (a1: MonoidAggregator[A1, B1, C1], a2: MonoidAggregator[A2, B2, C2], axs: List[Either[A1, A2]]) =>
         val a3 = ParallelAggregators.oneOf(a1 :: a2 :: HNil)
-        a3(axs.iterator.map(f)) shouldBe (a1 either a2)(axs).productElements
+        a3(axs.iterator.map(f)) shouldBe a1.either(a2)(axs).productElements
     }
   }
 }
@@ -87,7 +84,7 @@ abstract class ApplicativeAggregatorTest[
       val a3 = ApplicativeAggregators(a1 :: a2 :: HNil)
 
       val expected =
-        if (as.nonEmpty) (a1 join a2)(as).productElements
+        if (as.nonEmpty) a1.join(a2)(as).productElements
         else a1.present(Monoid.zero[B1]) :: a2.present(Monoid.zero[B2]) :: HNil
 
       a3(as) shouldBe expected
@@ -95,8 +92,7 @@ abstract class ApplicativeAggregatorTest[
   }
 }
 
-class ApplicativeAggregator_IBSLL
-    extends ApplicativeAggregatorTest[Int, BigInt, String, Long, Long]("IBSLL")
+class ApplicativeAggregator_IBSLL extends ApplicativeAggregatorTest[Int, BigInt, String, Long, Long]("IBSLL")
 
 abstract class JoinedAggregatorTest[
     A1: Arbitrary: Cogen,
@@ -110,29 +106,25 @@ abstract class JoinedAggregatorTest[
     with PropertyChecks {
 
   property(s"$name: joined aggregator product (allOf)") {
-    forAll {
-      (a1: MonoidAggregator[A1, B1, C], a2: MonoidAggregator[A2, B2, C], as: List[(A1, A2)]) =>
-        val a3 = JoinedAggregators.allOf(a1 :: a2 :: HNil)
-        val got = a3(as.map(_.productElements))
-        val expected = Monoid.plus(a1(as.map(_._1)), a2(as.map(_._2)))
-        got shouldBe expected
+    forAll { (a1: MonoidAggregator[A1, B1, C], a2: MonoidAggregator[A2, B2, C], as: List[(A1, A2)]) =>
+      val a3 = JoinedAggregators.allOf(a1 :: a2 :: HNil)
+      val got = a3(as.map(_.productElements))
+      val expected = Monoid.plus(a1(as.map(_._1)), a2(as.map(_._2)))
+      got shouldBe expected
     }
   }
 
   property(s"$name: joined aggregator product (oneOf)") {
-    forAll {
-      (a1: MonoidAggregator[A1, B1, C],
-       a2: MonoidAggregator[A2, B2, C],
-       as: List[Either[A1, A2]]) =>
-        val a3 = JoinedAggregators.oneOf(a1 :: a2 :: HNil)
-        val got = a3(as.map {
-          case Left(a)  => Inl(a)
-          case Right(x) => Inr(Inl(x))
-        })
-        val expected = Monoid.plus(a1(as.collect { case Left(a) => a }), a2(as.collect {
-          case Right(x)                                         => x
-        }))
-        got shouldBe expected
+    forAll { (a1: MonoidAggregator[A1, B1, C], a2: MonoidAggregator[A2, B2, C], as: List[Either[A1, A2]]) =>
+      val a3 = JoinedAggregators.oneOf(a1 :: a2 :: HNil)
+      val got = a3(as.map {
+        case Left(a)  => Inl(a)
+        case Right(x) => Inr(Inl(x))
+      })
+      val expected = Monoid.plus(a1(as.collect { case Left(a) => a }), a2(as.collect {
+        case Right(x)                                         => x
+      }))
+      got shouldBe expected
     }
   }
 }
@@ -171,37 +163,32 @@ abstract class CinchedAggregatorTest[
     with PropertyChecks {
 
   property(s"$name: cinched aggregator product (allOf)") {
-    forAll {
-      (a1: MonoidAggregator[A1, B, C1], a2: MonoidAggregator[A2, B, C2], as: List[(A1, A2)]) =>
-        val a3 = CinchedAggregators.allOf(a1 :: a2 :: HNil)
-        val got = a3(as.map(_.productElements))
-        val b = Monoid.sum(as.map { case (x1, x2) => Monoid.plus(a1.prepare(x1), a2.prepare(x2)) })
-        val expected = a1.present(b) :: a2.present(b) :: HNil
-        got shouldBe expected
+    forAll { (a1: MonoidAggregator[A1, B, C1], a2: MonoidAggregator[A2, B, C2], as: List[(A1, A2)]) =>
+      val a3 = CinchedAggregators.allOf(a1 :: a2 :: HNil)
+      val got = a3(as.map(_.productElements))
+      val b = Monoid.sum(as.map { case (x1, x2) => Monoid.plus(a1.prepare(x1), a2.prepare(x2)) })
+      val expected = a1.present(b) :: a2.present(b) :: HNil
+      got shouldBe expected
     }
   }
 
   property(s"$name: cinched aggregator product (oneOf)") {
-    forAll {
-      (a1: MonoidAggregator[A1, B, C1],
-       a2: MonoidAggregator[A2, B, C2],
-       as: List[Either[A1, A2]]) =>
-        val a3 = CinchedAggregators.oneOf(a1 :: a2 :: HNil)
-        val got = a3(as.map {
-          case Left(a)  => Inl(a)
-          case Right(x) => Inr(Inl(x))
-        })
-        val resB = a1.monoid.sum(as.map {
-          case Left(x)  => a1.prepare(x)
-          case Right(x) => a2.prepare(x)
-        })
-        val expected = a1.present(resB) :: a2.present(resB) :: HNil
-        got shouldBe expected
+    forAll { (a1: MonoidAggregator[A1, B, C1], a2: MonoidAggregator[A2, B, C2], as: List[Either[A1, A2]]) =>
+      val a3 = CinchedAggregators.oneOf(a1 :: a2 :: HNil)
+      val got = a3(as.map {
+        case Left(a)  => Inl(a)
+        case Right(x) => Inr(Inl(x))
+      })
+      val resB = a1.monoid.sum(as.map {
+        case Left(x)  => a1.prepare(x)
+        case Right(x) => a2.prepare(x)
+      })
+      val expected = a1.present(resB) :: a2.present(resB) :: HNil
+      got shouldBe expected
     }
   }
 }
 
-class CinchedAggregator_ISLSB
-    extends CinchedAggregatorTest[Int, String, Long, String, BigInt]("ISLSB")
+class CinchedAggregator_ISLSB extends CinchedAggregatorTest[Int, String, Long, String, BigInt]("ISLSB")
 
 class CinchedAggregator_IIIII extends CinchedAggregatorTest[Int, Int, Int, Int, Int]("IIIII")
