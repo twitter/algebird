b8074cfbe8bfe10d0cf1a88433fa063f98f1d0d7
César Collé <cesar.colle@gmail.com>
2018-08-23 17:00:03 +0200

    Add fingerprint generation + hash value with XOR

--- algebird-core/src/main/scala/com/twitter/algebird/CuckooFilter.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/CuckooFilter.scala
@@ -2,64 +2,69 @@ package com.twitter.algebird
 
 import algebra.BoundedSemilattice
 
+import scala.collection.BitSet
+
 /**
- * Cuckoo filter for Algebird in a Monoid way of life.
- * referring :
- *  - https://github.com/twitter/algebird/issues/560#issue-185161455
- *  - http://www.eecs.harvard.edu/%7Emichaelm/postscripts/cuckoo-conext2014.pdf
- *  - https://github.com/newhoggy/pico-cuckoo-filter/blob/master/pico-cuckoo-filter/src/main/scala/org/pico/cuckoo/filter/CuckooFilter.scala
- * By nature, this filter isn't commutative
- *
- * */
+  * Cuckoo filter for Algebird in a Monoid way of life.
+  * referring :
+  *  - https://github.com/twitter/algebird/issues/560#issue-185161455
+  *  - http://www.eecs.harvard.edu/%7Emichaelm/postscripts/cuckoo-conext2014.pdf
+  *  - https://github.com/newhoggy/pico-cuckoo-filter/blob/master/pico-cuckoo-filter/src/main/scala/org/pico/cuckoo/filter/CuckooFilter.scala
+  * By nature, this filter isn't commutative
+  *
+  **/
+
 
 object CuckooFilter {
-// TODO : optimal parameter have to be compute here : see Asymptotic behavior of the original paper.
+  // TODO : optimal parameter have to be compute here : see Asymptotic behavior of the original paper.
   def apply[A](bucketSize: Int, fingerPrintSize: LongBitSet, kicks: Int, buckets: Int)(
-      implicit hash1: Hash128[A],
-      hash2: Hash128[A]): CuckooFilterMonoid[A] = {
+    implicit hash1: Hash128[A],
+    hash2: Hash128[A]): CuckooFilterMonoid[A] = {
     null
   }
 }
 
 /**
   * The cuckoo filter monoid
-  * */
+  **/
 // TODO : Add the fingerprint size.
 case class CuckooFilterMonoid[A](fingerprintBucket: Int, maxKicks: Int = 3, totalBuckets: Int = 256)(
-    implicit h1: Hash128[A],
-    h2: Hash128[A])
-    extends Monoid[CF[A]]
-    with BoundedSemilattice[BF[A]] {
+  implicit h1: Hash128[A])
+  extends Monoid[CF[A]]
+    with BoundedSemilattice[CF[A]] {
   override def zero: CF[A] = ???
 
   override def plus(x: CF[A], y: CF[A]): CF[A] = ???
 
   /**
-   * Create a cuckoo filter with one item.
-   */
+    * Create a cuckoo filter with one item.
+    */
   def create(item: A): BF[A] = ???
 
   /**
-   * Create a cuckoo filter with multiple items.
-   */
+    * Create a cuckoo filter with multiple items.
+    */
   def create(data: A*): BF[A] = create(data.iterator)
 
   /**
-   * Create a cuckoo filter with multiple items from an iterator
-   */
+    * Create a cuckoo filter with multiple items from an iterator
+    */
   def create(data: Iterator[A]): BF[A] = ???
 
 }
 
 /**
- * Cuckoo data structure abstract
- */
+  * Cuckoo data structure abstract
+  */
 sealed abstract class CF[A] extends java.io.Serializable {
 
   def ++(other: CF[A]): CF[A]
 
   def +(other: A): CF[A]
 
+  def bucketBits : Int
+
+  def bucketIndex(hash : Long) : Int
 
   def checkAndAdd(item: A): (CF[A], ApproximateBoolean)
 
@@ -79,13 +84,15 @@ sealed abstract class CF[A] extends java.io.Serializable {
 
   def density = numBits.toDouble / width
 
+  
+
   def size: Approximate[Long]
 }
 
 /**
   * Empty cuckoo filter
-  * */
-case class CFZero[A](fingerPrintBucket : Int, maxKicks : Int = 3, totalBuckets : Int = 256) extends CF[A] {
+  **/
+case class CFZero[A](fingerPrintBit :Int, fingerPrintBucket: Int, maxKicks: Int = 3, totalBuckets: Int = 256) extends CF[A] {
 
   override def checkAndAdd(item: A): (CF[A], ApproximateBoolean) = ???
 
@@ -99,53 +106,94 @@ case class CFZero[A](fingerPrintBucket : Int, maxKicks : Int = 3, totalBuckets :
 
   override def ++(other: CF[A]): CF[A] = other
 
-  override def +(other: A): CF[A] = new CFItem[A](other, fingerPrintBucket, maxKicks = maxKicks, totalBuckets = totalBuckets)
+  override def +(other: A): CF[A] = new CFItem[A](other, new CFHash[A](), fingerPrintBit, fingerPrintBucket, maxKicks = maxKicks, totalBuckets = totalBuckets)
+
+  override def bucketBits: Int = 0
+
+  override def bucketIndex(hash: Long): Int = 0
 }
 
 /**
   * One item cuckoo
-  * */
+  **/
 
-case class CFItem[A](item: A,fingerPrintBucket : Int, maxKicks : Int = 3, totalBuckets : Int = 256 ) extends CF[A] {
+case class CFItem[A](item: A, cFHash: CFHash[A], fingerprintBucket : Int, fingerprintBit: Int, maxKicks: Int = 3, totalBuckets: Int = 256) extends CF[A] {
 
   override def checkAndAdd(item: A): (CF[A], ApproximateBoolean) = ???
+
   override def maybeContains(item: A): Boolean = ???
-  override def numBits: Int = ???
+
+  override def numBits: Int = cFHash.generateFingerPrint(cFHash(item)(1))
+
   override def width: Int = ???
+
   override def size: Approximate[Long] = ???
-  override def ++(other: CF[A]): CF[A] = CFInstance()
-  override def +(other: A): CF[A] = this ++ CFItem[A](other, fingerPrintBucket, maxKicks = maxKicks, totalBuckets = totalBuckets)
+
+  override def ++(other: CF[A]): CF[A] = ??? //CFInstance()
+  override def +(other: A): CF[A] = this ++ CFItem[A](other,cFHash, fingerprintBucket, fingerprintBit, maxKicks, totalBuckets)
+
+  override def bucketBits: Int = ???
+
+  override def bucketIndex(hash: Long): Int = ???
 }
 
 /**
   * Multiple items cuckoo
-  * */
+  **/
 
-case class CFInstance[A]() extends CF[A] {
+case class CFInstance[A](hash: CFHash[A],
+                         cuckooBitSet: Array[BitSet],
+                         fingerprintBit : Int,
+                         fingerprintBucket: Int,
+                         totalBuckets: Int) extends CF[A] {
+
   override def checkAndAdd(item: A): (CF[A], ApproximateBoolean) = ???
+
   override def maybeContains(item: A): Boolean = ???
-  override def numBits: Int = ???
+
+  override def numBits: Int = {
+    cuckooBitSet.foldLeft(0)((z, b) => z + b.size)
+  }
+
   override def width: Int = ???
+
   override def size: Approximate[Long] = ???
+
   override def ++(other: CF[A]): CF[A] = ???
-  override def +(other: A): CF[A] = ???
+
+  override def +(other: A): CFInstance[A] = {
+    val firstHash = hash(1, other)
+    val cuckooBS = Array(fingerprintBucket, LongBitSet.empty(fingerprintBucket * fingerprintBit))
+
+
+
+
+    null
+  }
+
+  override def bucketBits: Int = fingerprintBit * fingerprintBucket
+
+  override def bucketIndex(hash: Long): Int = ???
 }
 
 
 /**
   * The hash class for cuckoo
-  * */
-case class CFHash[A]()(implicit hash1: Hash128[A], hash2: Hash128[A]) {
+  **/
+private[algebird] case class CFHash[A](fingerprintSize : Int)(implicit hash: Hash128[A]) {
 
-  def apply(valueToHash: A)(implicit hash1: Hash128[A], hash2: Hash128[A]): Array[Int] = {
-    println(hash1.hash(valueToHash), "hash2 :: " + hash2.hash(valueToHash))
-    Array(hash1.hash(valueToHash)._1.toInt, hash2.hash(valueToHash)._1.toInt)
+  def apply(seed: Long, valueToHash: A): Array[Long] = {
+    println(hash.hashWithSeed(seed, valueToHash))
+    Array(hash.hashWithSeed(seed, valueToHash)._1, hash.hashWithSeed(seed, valueToHash)._2)
   }
 
+  // TODO: size fo fingerprint goes here
+  // took from https://github.com/efficient/cuckoofilter/blob/aac6569cf30f0dfcf39edec1799fc3f8d6f594da/src/cuckoofilter.h#L66
+  def generateFingerPrint(f: Long ): Byte = {
+    var fingerprint: Byte = (f & 1 << fingerprintSize).toByte
+    //fingerprint +=  (fingerprint == 0).asInstanceOf[Byte]
 
-
-}
-// TODO : here is for setting fingerprint size.
-case class CFFingerprint[A](){
+    fingerprint
+  }
 
 }
--- algebird-test/src/test/scala/com/twitter/algebird/CuckooFilterTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/CuckooFilterTest.scala
@@ -4,17 +4,31 @@ import org.scalatest.{Matchers, WordSpec}
 class CuckooFilterTest extends WordSpec with Matchers {
 
 
-  "a cuckoo filter " in {
+  "a cuckoo filter " should {
 
     "have to choose the first bucket he need " in {
     }
   }
 
-  "a cuckoo hash" in {
+  "a cuckoo hash" should {
 
     "produce 1 hash value" in {
       val cuckooHash = CFHash[String]()
-      val hashed = cuckooHash("elmt")
+      val hashed = cuckooHash(1, "elmt")
+      println("leading 0 in 5 ::" + Integer.numberOfLeadingZeros(16))
+      println("leading 0 in 32 ::" + Integer.numberOfLeadingZeros(32))
+      println("leading 0 in 64 ::" + Integer.numberOfLeadingZeros(64))
+      println("leading 0 in 128::" + Integer.numberOfLeadingZeros(128))
+
+      println("Value : " + hashed(1))
+      println("hash % and the rest  " + (hashed(1) & 0x7fffffffffffffffL))
+
+
+      val buffer  = new Array[Byte]( 10 * 14)
+
+
+      val f = 32 - Integer.numberOfLeadingZeros(5)
+      println("leading 0 ::" + f)
       assert(hashed.length == 2)
     }
 
