96a7cb3ccf86c17932b84683b6d93813aaf99eb8
Jeff Sarnat <jeff@stripe.com>
2019-11-06 15:13:05 -0800

    new combinators and tests + renaming some methods from zip to join for consistency

--- algebird-core/src/main/scala/com/twitter/algebird/Scan.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Scan.scala
@@ -1,17 +1,57 @@
 package com.twitter.algebird
 
-import scala.collection.AbstractIterator
+import scala.collection.compat._
 import scala.collection.generic.CanBuildFrom
 
 object Scan {
+  /**
+    * Most consumers of Scan don't care about the type of the {{State}} type variable. But for those that do,
+    * we make an effort to expose it in all of our combinators.
+    * @tparam I
+    * @tparam S
+    * @tparam O
+    */
+  type Aux[-I, S, +O] = Scan[I, O] { type State = S }
+
   implicit def applicative[I]: Applicative[({ type L[O] = Scan[I, O] })#L] = new ScanApplicative[I]
 
-  def from[I, S, O](initState: S)(presentAndNextStateFn: (I, S) => (O, S)): Scan[I, O] =
+  def from[I, S, O](initState: S)(presentAndNextStateFn: (I, S) => (O, S)): Aux[I, S, O] =
     new Scan[I, O] {
       override type State = S
       override val initialState = initState
       override def presentAndNextState(i: I, s: State): (O, State) = presentAndNextStateFn(i, s)
     }
+
+  def fromFunction[I, O](f: I => O): Aux[I, Unit, O] = new Scan[I, O] {
+    override type State = Unit
+    override val initialState = ()
+    override def presentAndNextState(i: I, stateBeforeProcessingI: Unit): (O, State) = (f(i), ())
+  }
+
+  /**
+    * Streams can be thought of as being a hidden state that is queryable for a head element, and another hidden state
+    * that represents the rest of the stream. Scans take streams of inputs to streams of outputs, but some scans
+    * have trivial inputs and just produce a stream of outputs.
+    * @param initState The initial state of the scan; think of this as an infinite stream.
+    * @param destructor This function decomposes a stream into the its head-element and tail-stream.
+    * @tparam S The hidden state of the stream that we are turning into a Scan.
+    * @tparam O The type of the elments of the stream that we are turning into a Scan
+    * @return A Scan whose inputs are irrelevant, and whose outputs are those that we would get from implementing
+    *         a stream using the information provided to this method.
+    */
+  def fromStreamLike[S, O](initState: S)(destructor: S => (O, S)): Aux[Any, S, O] = new Scan[Any, O] {
+    override type State = S
+    override val initialState = initState
+    override def presentAndNextState(i: Any, stateBeforeProcessingI: S): (O, S) =
+      destructor(stateBeforeProcessingI)
+  }
+
+  /**
+    * A Scan that returns the number N for the Nth input (starting from 0)
+    */
+  val index: Aux[Any, Long, Long] = fromStreamLike(0L)(n => (n, n+1))
+
+  def identity[A] = fromFunction[A, A](x => x)
 
   /**
    *
@@ -22,7 +62,7 @@ object Scan {
    * @tparam O
    * @return A Scan that safely encapsulates state while it's doing its thing.
    */
-  def mutable[I, S, O](initStateCreator: => S)(presentAndUpdateStateFn: (I, S) => O): Scan[I, O] =
+  def mutable[I, S, O](initStateCreator: => S)(presentAndUpdateStateFn: (I, S) => O): Aux[I, S, O] =
     new Scan[I, O] {
       override type State = S
       override def initialState = initStateCreator
@@ -34,7 +74,7 @@ object Scan {
    * @param t
    * @tparam T
    */
-  def const[T](t: T): Scan[Any, T] = from(()) { (_, _) =>
+  def const[T](t: T): Aux[Any, Unit, T] = from(()) { (_, _) =>
     (t, ())
   }
 
@@ -48,7 +88,7 @@ object Scan {
    * @return A scan which, when given [a_1, ..., a_n] outputs [c_1, ..., c_n] where
    *         c_i = initState + aggregator.prepare(a_1) + ... + aggregator.prepare(a_i)
    */
-  def fromAggregator[A, B, C](aggregator: Aggregator[A, B, C], initState: B): Scan[A, C] =
+  def fromAggregator[A, B, C](aggregator: Aggregator[A, B, C], initState: B): Aux[A, B, C] =
     new Scan[A, C] {
       override type State = B
 
@@ -72,7 +112,7 @@ object Scan {
    * @return A scan which, when given [a_1, ..., a_n] outputs [c_1, ..., c_n] where
    *         c_i = monoidAggregator.monoid.zero + aggregator.prepare(a_1) + ... + aggregator.prepare(a_i)
    */
-  def fromMonoidAggregator[A, B, C](monoidAggregator: MonoidAggregator[A, B, C]): Scan[A, C] =
+  def fromMonoidAggregator[A, B, C](monoidAggregator: MonoidAggregator[A, B, C]): Aux[A, B, C] =
     fromAggregator(monoidAggregator, monoidAggregator.monoid.zero)
 
   /**
@@ -85,7 +125,7 @@ object Scan {
    * @return  A scan which, when given [a_1, ..., a_n] outputs [c_1, ..., c_n] where
    *         c_i = aggregator.prepare(a_i) + ... + aggregator.prepare(a_1) + initState
    */
-  def fromAggregatorReverse[A, B, C](aggregator: Aggregator[A, B, C], initState: B): Scan[A, C] =
+  def fromAggregatorReverse[A, B, C](aggregator: Aggregator[A, B, C], initState: B): Aux[A, B, C] =
     new Scan[A, C] {
       override type State = B
 
@@ -111,7 +151,7 @@ object Scan {
    */
   def fromMonoidAggregatorReverse[A, B, C](
       monoidAggregator: MonoidAggregator[A, B, C]
-  ): Scan[A, C] =
+  ): Aux[A, B, C] =
     fromAggregatorReverse(monoidAggregator, monoidAggregator.monoid.zero)
 
 }
@@ -132,6 +172,8 @@ object Scan {
  * @tparam O The output type of the scan (typically distinct from the hidden {{{State}}} of the scan.
  */
 sealed trait Scan[-I, +O] extends Serializable { self =>
+
+  import Scan.Aux
 
   /**
    * The computation of any given scan involves keeping track of a hidden state of type {{{State}}}
@@ -194,7 +236,7 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
 
   // combinators
 
-  def replaceState(newInitialState: => State): Scan[I, O] = new Scan[I, O] {
+  def replaceState(newInitialState: => State): Aux[I, State, O] = new Scan[I, O] {
     override type State = self.State
 
     override def initialState: State = newInitialState
@@ -203,7 +245,7 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
       self.presentAndNextState(i, stateBeforeProcessingI)
   }
 
-  def composePrepare[I1](f: I1 => I): Scan[I1, O] = new Scan[I1, O] {
+  def composePrepare[I1](f: I1 => I): Aux[I1, State, O] = new Scan[I1, O] {
     override type State = self.State
 
     override def initialState: State = self.initialState
@@ -212,7 +254,7 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
       self.presentAndNextState(f(i), stateBeforeProcessingI)
   }
 
-  def andThenPresent[O1](g: O => O1): Scan[I, O1] = new Scan[I, O1] {
+  def andThenPresent[O1](g: O => O1): Aux[I, State, O1] = new Scan[I, O1] {
     override type State = self.State
     override def initialState: State = self.initialState
 
@@ -222,7 +264,13 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
     }
   }
 
-  def zipWithInput[I1 <: I]: Scan[I1, (I1, O)] = new Scan[I1, (I1, O)] {
+  /**
+    *
+    * @tparam I1
+    * @return A scanner that is semantically identical to .join(Scan.identity[I1]), but without
+    *         where we don't pollute the {{State}} by pairing it redundantly with {{Unit}}.
+    */
+  def joinWithInput[I1 <: I]: Aux[I1, State, (I1, O)] = new Scan[I1, (I1, O)] {
     override type State = self.State
 
     override def initialState: State = self.initialState
@@ -233,7 +281,7 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
     }
   }
 
-  def zipWithPriorState: Scan[I, (State, O)] = new Scan[I, (State, O)] {
+  def joinWithPriorState: Aux[I, State, (State, O)] = new Scan[I, (State, O)] {
     override type State = self.State
 
     override def initialState: State = self.initialState
@@ -244,7 +292,7 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
     }
   }
 
-  def zipWithPosteriorState: Scan[I, (O, State)] = new Scan[I, (O, State)] {
+  def joinWithPosteriorState: Aux[I, State, (O, State)] = new Scan[I, (O, State)] {
     override type State = self.State
 
     override def initialState: State = self.initialState
@@ -255,7 +303,9 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
     }
   }
 
-  def zip[I2, O2](scan2: Scan[I2, O2]): Scan[(I, I2), (O, O2)] = new Scan[(I, I2), (O, O2)] {
+  def joinWithIndex: Aux[I, (State, Long), (O, Long)] = join(Scan.index)
+
+  def zip[I2, O2](scan2: Scan[I2, O2]): Aux[(I, I2), (State, scan2.State), (O, O2)] = new Scan[(I, I2), (O, O2)] {
     override type State = (self.State, scan2.State)
 
     override def initialState: State = (self.initialState, scan2.initialState)
@@ -273,7 +323,7 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
     }
   }
 
-  def join[I2 <: I, O2](scan2: Scan[I2, O2]): Scan[I2, (O, O2)] = new Scan[I2, (O, O2)] {
+  def join[I2 <: I, O2](scan2: Scan[I2, O2]): Aux[I2, (State, scan2.State), (O, O2)] = new Scan[I2, (O, O2)] {
     override type State = (self.State, scan2.State)
 
     override def initialState: State = (self.initialState, scan2.initialState)
@@ -282,6 +332,18 @@ sealed trait Scan[-I, +O] extends Serializable { self =>
       val (o1, state1AfterProcesingI1) = self.presentAndNextState(i, stateBeforeProcessingI._1)
       val (o2, state2AfterProcesingI2) = scan2.presentAndNextState(i, stateBeforeProcessingI._2)
       ((o1, o2), (state1AfterProcesingI1, state2AfterProcesingI2))
+    }
+  }
+
+  def compose[P](scan2: Scan[O, P]): Aux[I, (State, scan2.State), P] = new Scan[I, P] {
+    override type State = (self.State, scan2.State)
+
+    override def initialState: State = (self.initialState, scan2.initialState)
+
+    override def presentAndNextState(i: I, stateBeforeProcessingI: State): (P, State) = {
+      val (o, state1AfterProcesingI1) = self.presentAndNextState(i, stateBeforeProcessingI._1)
+      val (p, state2AfterProcesingI2) = scan2.presentAndNextState(o, stateBeforeProcessingI._2)
+      (p, (state1AfterProcesingI1, state2AfterProcesingI2))
     }
   }
 
--- algebird-test/src/test/scala/com/twitter/algebird/ScanTest.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/ScanTest.scala
@@ -8,11 +8,13 @@ import scala.collection.mutable.Queue
 
 
 object ScanTest {
+  // normal people will use Scan not Scan.Aux, so it's good for most of the tests to be using the more common interface.
   type StringScan = Scan[Char, String]
 
   // technically speaking, these aren't exactly the Free scanner, since that would output a giant tree structure from
   // the whole scan, but that giant tree structure is pretty close to a String.
-  val directFreeScan: StringScan = Scan.from(List.empty[Char]) { (char, previousState) =>
+
+  val directFreeScan: Scan.Aux[Char, List[Char], String] = Scan.from(List.empty[Char]) { (char, previousState) =>
     val nextState = char :: previousState
     (nextState.reverse.mkString, nextState)
   }
@@ -43,6 +45,25 @@ object ScanTest {
 
   }
 
+  val joinWithPosteriorStateFreeScan: StringScan =
+    directFreeScan
+      .andThenPresent(_ => ())
+      .joinWithPosteriorState
+      .andThenPresent { case ((), state) => state.reverse.mkString }
+
+  val joinWithPriorStateFreeScan1: StringScan =
+    directFreeScan
+    .andThenPresent(_ => ())
+    .joinWithPriorState
+    .joinWithInput
+    .andThenPresent{ case (input, (state, ())) => (input::state).mkString.reverse}
+
+  val joinWithPriorStateFreeScan2: StringScan =
+    directFreeScan
+    .andThenPresent(_ => ())
+    .joinWithPriorState
+    .join(Scan.identity[Char])
+    .andThenPresent{ case ((state, ()) , input) => (input::state).mkString.reverse}
 
 }
 
@@ -79,6 +100,22 @@ class ScanTest extends WordSpec with Matchers with GeneratorDrivenPropertyChecks
       freeScanLaws(reverseAggregatorFreeScan)
     }
 
+    "be obeyed by an implementation of the almost-free Scan using a direct implementation, andThenPresent, and joinWithPosteriorState" in {
+      freeScanLaws(joinWithPosteriorStateFreeScan)
+    }
+
+    "be obeyed by an implementation of the almost-free Scan using a direct implmeentation, andThenPresent, joinWithPriorState, and joinWithInput" in {
+      freeScanLaws(joinWithPriorStateFreeScan1)
+    }
+
+    "be obeyed by an implementation of the almost-free Scan using a direct implmeentation, andThenPresent, joinWithPriorState, and join with scan.Identity" in {
+      freeScanLaws(joinWithPriorStateFreeScan2)
+    }
+
+    "be obeyed by composing the identity scan on either side of a direct-implementation of the almost-free Scan" in {
+      freeScanLaws(Scan.identity.compose(directFreeScan))
+      freeScanLaws(directFreeScan.compose(Scan.identity))
+    }
 
   }
 
