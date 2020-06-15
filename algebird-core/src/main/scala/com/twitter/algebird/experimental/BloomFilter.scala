35abad4a7fda68a66f58196929b8647b834def44
Filipe Regadas <filiperegadas@gmail.com>
2020-06-15 17:31:27 +0100

    Add first attempt on path dependent bloom filter

---
+++ algebird-core/src/main/scala/com/twitter/algebird/experimental/BloomFilter.scala
@@ -0,0 +1,397 @@
+/*
+Copyright 2020 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.twitter.algebird.experimental
+
+import algebra.BoundedSemilattice
+import com.twitter.algebird.immutable.BitSet
+import com.twitter.algebird.{Approximate, ApproximateBoolean, Hash128, Monoid, MonoidAggregator}
+
+import scala.collection.compat._
+
+object BloomFilter {
+
+  final def apply[A](numEntries: Int, fpProb: Double)(implicit hash: Hash128[A]): BloomFilter[A] =
+    BloomFilter.optimalWidth(numEntries, fpProb) match {
+      case None =>
+        throw new java.lang.IllegalArgumentException(
+          s"BloomFilter cannot guarantee the specified false positive probability for the number of entries! (numEntries: $numEntries, fpProb: $fpProb)"
+        )
+      case Some(width) =>
+        val numHashes = BloomFilter.optimalNumHashes(numEntries, width)
+        new BloomFilter(numHashes, width)
+    }
+
+  // Compute optimal number of hashes: k = m/n ln(2)
+  def optimalNumHashes(numEntries: Int, width: Int): Int =
+    math.ceil(width / numEntries * math.log(2)).toInt
+
+  // Compute optimal width: m = - n ln(p) / (ln(2))^2
+  // return None if we can't meet this false positive probability
+  def optimalWidth(numEntries: Int, fpProb: Double): Option[Int] = {
+    val widthEstimate = math
+      .ceil(-1 * numEntries * math.log(fpProb) / math.log(2) / math.log(2))
+      .toInt
+
+    if (widthEstimate == Int.MaxValue) None
+    else Some(widthEstimate)
+  }
+
+  /**
+   * Cardinality estimates are taken from Theorem 1 on page 15 of
+   * "Cardinality estimation and dynamic length adaptation for Bloom filters"
+   * by Papapetrou, Siberski, and Nejdl:
+   * http://www.softnet.tuc.gr/~papapetrou/publications/Bloomfilters-DAPD.pdf
+   *
+   * Roughly, by using bounds on the expected number of true bits after n elements
+   * have been inserted into the Bloom filter, we can go from the actual number of
+   * true bits (which is known) to an estimate of the cardinality.
+   *
+   * approximationWidth defines an interval around the maximum-likelihood cardinality
+   * estimate. Namely, the approximation returned is of the form
+   * (min, estimate, max) =
+   *   ((1 - approxWidth) * estimate, estimate, (1 + approxWidth) * estimate)
+   */
+  def sizeEstimate(
+      numBits: Int,
+      numHashes: Int,
+      width: Int,
+      approximationWidth: Double = 0.05
+  ): Approximate[Long] = {
+    assert(0 <= approximationWidth && approximationWidth < 1, "approximationWidth must lie in [0, 1)")
+
+    /**
+     * s(n) is the expected number of bits that have been set to true after
+     * n elements have been inserted into the Bloom filter.
+     * This is \hat{S}(n) in the cardinality estimation paper used above.
+     */
+    def s(n: Int): Double =
+      width * (1 - scala.math.pow(1 - 1.0 / width, numHashes * n))
+
+    /**
+     * sInverse(t) is the maximum likelihood value for the number of elements
+     * that have been inserted into the Bloom filter when it has t bits set to true.
+     * This is \hat{S}^{-1}(t) in the cardinality estimation paper used above.
+     */
+    def sInverse(t: Int): Double =
+      scala.math.log1p(-t.toDouble / width) / (numHashes * scala.math.log1p(-1.0 / width))
+
+    // Variable names correspond to those used in the paper.
+    val t = numBits
+    val n = sInverse(t).round.toInt
+    // Take the min and max because the probability formula assumes
+    // nl <= sInverse(t - 1) and sInverse(t + 1) <= nr
+    val nl =
+      scala.math.min(sInverse(t - 1).floor, (1 - approximationWidth) * n).toInt
+    val nr =
+      scala.math.max(sInverse(t + 1).ceil, (1 + approximationWidth) * n).toInt
+    val prob =
+      1 -
+        scala.math.exp(t - 1 - s(nl)) *
+          scala.math.pow(s(nl) / (t - 1), t - 1) -
+        scala.math.exp(-scala.math.pow(t + 1 - s(nr), 2) / (2 * s(nr)))
+
+    Approximate[Long](nl, n, nr, scala.math.max(0, prob))
+  }
+}
+
+/**
+ * Bloom Filter - a probabilistic data structure to test presence of an element.
+ *
+ * Operations
+ *   1) insert: hash the value k times, updating the bitfield at the index equal to each hashed value
+ *   2) query: hash the value k times.  If there are k collisions, then return true; otherwise false.
+ *
+ * http://en.wikipedia.org/wiki/Bloom_filter
+ *
+ */
+final case class BloomFilter[A](n: Int, w: Int)(implicit hash: Hash128[A]) {
+  case object Hash {
+    final def apply(s: A): Array[Int] =
+      nextHash(s, 0, new Array[Int](4), 4, new Array[Int](n))
+
+    private def splitLong(x: Long, buffer: Array[Int], idx: Int): Unit = {
+      // unfortunately, this is the function we committed to some time ago, and we have tests
+      // locking it down. x.toInt & 0x7fffffff should work, but this gives a few different values
+      def toNonNegativeInt(x: Long): Int =
+        (math
+          .abs(x)
+          .toInt) & 0x7fffffff // no change for positive numbers, converts Integer.MIN_VALUE to positive number
+
+      val upper = toNonNegativeInt(x >> 32)
+      val lower = toNonNegativeInt((x << 32) >> 32)
+      buffer(idx) = upper
+      buffer(idx + 1) = lower
+    }
+
+    @annotation.tailrec
+    private def nextHash(
+        valueToHash: A,
+        hashIndex: Int,
+        buffer: Array[Int],
+        bidx: Int,
+        target: Array[Int]
+    ): Array[Int] =
+      if (hashIndex == n) target
+      else {
+        val thisBidx = if (bidx > 3) {
+          val (a, b) =
+            hash.hashWithSeed((n - hashIndex).toLong, valueToHash)
+          splitLong(a, buffer, 0)
+          splitLong(b, buffer, 2)
+          0
+        } else bidx
+
+        target(hashIndex) = buffer(thisBidx) % w
+        nextHash(valueToHash, hashIndex + 1, buffer, thisBidx + 1, target)
+      }
+  }
+
+  /**
+   * Bloom Filter data structure
+   */
+  sealed abstract class BF extends Serializable {
+    val numHashes: Int = n
+
+    val width: Int = w
+
+    /**
+     * The number of bits set to true in the bloom filter
+     */
+    def numBits: Int
+
+    /**
+     * Proportion of bits that are set to true.
+     */
+    final val density: Double = numBits.toDouble / width
+
+    def ++(other: BF): BF
+
+    def +(other: A): BF
+
+    def checkAndAdd(item: A): (BF, ApproximateBoolean)
+
+    def contains(item: A): ApproximateBoolean =
+      if (maybeContains(item)) {
+        // The false positive probability (the probability that the Bloom filter erroneously
+        // claims that an element x is in the set when x is not) is roughly
+        // p = (1 - e^(-numHashes * setCardinality / width))^numHashes
+        // See: http://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
+        //
+        // However, the true set cardinality may not be known. From empirical evidence, though,
+        // it is upper bounded with high probability by 1.1 * estimatedCardinality (as long as the
+        // Bloom filter is not too full), so we plug this into the formula instead.
+        // TODO: investigate this upper bound and density more closely (or derive a better formula).
+        val fpProb =
+          if (density > 0.95)
+            1.0 // No confidence in the upper bound on cardinality.
+          else
+            scala.math.pow(1 - scala.math.exp(-numHashes * size.estimate * 1.1 / width), numHashes)
+
+        ApproximateBoolean(true, 1 - fpProb)
+      } else {
+        // False negatives are not possible.
+        ApproximateBoolean.exactFalse
+      }
+
+    /**
+     * This may be faster if you don't care about evaluating
+     * the false positive probability
+     */
+    def maybeContains(item: A): Boolean
+
+    // Estimates the cardinality of the set of elements that have been
+    // inserted into the Bloom Filter.
+    def size: Approximate[Long]
+
+    def toBitSet: BitSet
+
+    /**
+     * Compute the Hamming distance between the two Bloom filters
+     * `a` and `b`. The distance is defined as the number of bits that
+     * need to change to in order to transform one filter into the other.
+     */
+    def hammingDistance(that: BF): Int =
+      (this, that) match {
+        // Comparing with empty filter should give number
+        // of bits in other set
+        case (_: Zero.type, _: Zero.type) => 0
+        case (_: Zero.type, y: BF)        => y.numBits
+        case (x: BF, _: Zero.type)        => x.numBits
+
+        // Otherwise compare as bit sets
+        case (_, _) => (this.toBitSet ^ that.toBitSet).size.toInt
+      }
+
+  }
+
+  case object Zero extends BF {
+    override def toBitSet: BitSet = BitSet()
+
+    override val numBits = 0
+
+    override def ++(other: BF): BF = other
+
+    override def +(other: A): Item = Item(other)
+
+    override def checkAndAdd(other: A): (BF, ApproximateBoolean) =
+      (this + other, ApproximateBoolean.exactFalse)
+
+    override def contains(item: A): ApproximateBoolean = ApproximateBoolean.exactFalse
+
+    override def maybeContains(item: A): Boolean = false
+
+    override def size: Approximate[Long] = Approximate.exact[Long](0)
+  }
+
+  case class Item(item: A) extends BF {
+    override val numBits: Int = numHashes
+
+    override def toBitSet: BitSet = BitSet(Hash(item))
+
+    override def ++(other: BF): BF =
+      other match {
+        case _: Zero.type    => this
+        case Item(otherItem) => Instance(BitSet(Hash(item) ++ Hash(otherItem)))
+        case _               => other + item
+      }
+
+    override def +(other: A): BF = this ++ Item(other)
+
+    override def checkAndAdd(other: A): (BF, ApproximateBoolean) =
+      if (other == item) {
+        (this, ApproximateBoolean.exactTrue)
+      } else {
+        (this + other, ApproximateBoolean.exactFalse)
+      }
+
+    override def contains(x: A): ApproximateBoolean = ApproximateBoolean.exact(item == x)
+
+    override def maybeContains(x: A): Boolean = item == x
+
+    override def size: Approximate[Long] = Approximate.exact[Long](1)
+  }
+
+  /*
+   * Bloom filter with multiple values
+   */
+  case class Instance(bits: BitSet) extends BF {
+
+    /**
+     * The number of bits set to true
+     */
+    override def numBits: Int = bits.size.toInt
+
+    override val toBitSet: BitSet = bits
+
+    override def ++(other: BF): BF =
+      other match {
+        case _: Zero.type        => this
+        case Item(item)          => this + item
+        case Instance(otherBits) => Instance(bits | otherBits)
+      }
+
+    override def +(item: A): Instance = Instance(bits | BitSet(Hash(item)))
+
+    override def checkAndAdd(other: A): (BF, ApproximateBoolean) =
+      (this + other, contains(other))
+
+    override def maybeContains(item: A): Boolean = {
+      val il = Hash(item)
+      var idx = 0
+      while (idx < il.length) {
+        val i = il(idx)
+        if (!bits(i)) return false
+        idx += 1
+      }
+      true
+    }
+
+    // use an approximation width of 0.05
+    override def size: Approximate[Long] =
+      BloomFilter.sizeEstimate(numBits, numHashes, width, 0.05)
+  }
+
+  implicit val instances =
+    new Monoid[BF] with BoundedSemilattice[BF] with Equiv[BF] with MonoidAggregator[A, BF, BF] {
+      override val zero: BF = Zero
+
+      /**
+       * Assume the bloom filters are compatible (same width and same hashing functions).  This
+       * is the union of the 2 bloom filters.
+       */
+      override def plus(left: BF, right: BF): BF = left ++ right
+
+      override def sumOption(as: TraversableOnce[BF]): Option[BF] =
+        if (as.iterator.isEmpty) None
+        else {
+          var bfItem: Item = null
+          val iter = as.iterator
+          var counter = 0
+          var bs = BitSet.newEmpty(0)
+
+          while (iter.hasNext) {
+            iter.next() match {
+              case _: Zero.type => ()
+              case bi @ Item(item) =>
+                bfItem = bi
+                val array = Hash(item)
+                counter += array.length
+                bs = bs.mutableAdd(array)
+              case Instance(bitset) =>
+                val iter = bitset.iterator
+                while (iter.hasNext) {
+                  counter += 1
+                  bs = bs.mutableAdd(iter.next())
+                }
+            }
+          }
+
+          if (counter == 0) Some(zero)
+          else if (counter == n && (bfItem != null)) Some(bfItem)
+          else Some(Instance(bs))
+        }
+
+      override def equiv(a: BF, b: BF): Boolean =
+        (a eq b) || ((a.numHashes == b.numHashes) &&
+          (a.width == b.width) &&
+          a.toBitSet.equals(b.toBitSet))
+
+      override val monoid: Monoid[BF] = this
+
+      override def prepare(value: A): BF = Item(value)
+
+      override def present(bf: BF): BF = bf
+    }
+
+  /**
+   * Create a bloom filter with one item.
+   */
+  def create(item: A): BF = Item(item)
+
+  /**
+   * Create a bloom filter with multiple items.
+   */
+  def create(data: A*): BF = create(data.iterator)
+
+  /**
+   * Create a bloom filter with multiple items from an iterator
+   */
+  def create(data: Iterator[A]): BF = instances.sum(data.map(Item(_)))
+
+  val zero: BF = Zero
+
+}
---
+++ algebird-test/src/test/scala/com/twitter/algebird/experimental/BloomFilterTest.scala
@@ -0,0 +1,429 @@
+package com.twitter.algebird.experimental
+
+import java.io.{ByteArrayOutputStream, ObjectOutputStream}
+
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Prop._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import com.twitter.algebird.immutable.BitSet
+import com.twitter.algebird.{
+  Aggregator,
+  ApproximateProperties,
+  ApproximateProperty,
+  Bytes,
+  CheckProperties,
+  Hash128,
+  Monoid,
+  MurmurHash128
+}
+
+object BloomFilterTestUtils {
+  def toDense[A](bloomFilter: BloomFilter[A])(bf: bloomFilter.BF): bloomFilter.Instance = bf match {
+    case _: bloomFilter.Zero.type => bloomFilter.Instance(BitSet.empty)
+    case bloomFilter.Item(item) =>
+      val bs = bloomFilter.Hash(item).foldLeft(BitSet.empty)(_ + _)
+      bloomFilter.Instance(bs)
+    case bfi: bloomFilter.Instance => bfi
+  }
+}
+
+class ExperimentalBloomFilterLaws extends CheckProperties {
+
+  import com.twitter.algebird.BaseProperties._
+  import BloomFilterTestUtils._
+
+  val bf: BloomFilter[String] = BloomFilter[String](6, 12)
+  import bf._
+
+  implicit val bfGen: Arbitrary[bf.BF] =
+    Arbitrary {
+      val item = Gen.choose(0, 10000).map(v => bf.create(v.toString))
+      val zero = Gen.const(Monoid.zero)
+      val sparse = Gen.listOf(item).map { its =>
+        Monoid.sum(its)
+      }
+      val dense = Gen.listOf(item).map { its =>
+        toDense(bf)(Monoid.sum(its))
+      }
+      Gen.frequency((1, zero), (5, item), (10, sparse), (10, dense))
+    }
+
+  property("BloomFilter is a Monoid") {
+    commutativeMonoidLaws[bf.BF]
+  }
+
+  property("++ is the same as plus") {
+    forAll((a: bf.BF, b: bf.BF) => Equiv[bf.BF].equiv(a ++ b, Monoid.plus(a, b)))
+  }
+
+  property("the distance between a filter and itself should be 0") {
+    forAll((a: bf.BF) => a.hammingDistance(a) == 0)
+  }
+
+  property(
+    "the distance between a filter and an empty filter should be the number of bits" +
+      "set in the existing filter"
+  ) {
+    forAll((a: bf.BF) => a.hammingDistance(Monoid.zero) == a.numBits)
+  }
+
+  property("all equivalent filters should have 0 Hamming distance") {
+    forAll { (a: bf.BF, b: bf.BF) =>
+      if (Equiv[bf.BF].equiv(a, b))
+        a.hammingDistance(b) == 0
+      else {
+        val dist = a.hammingDistance(b)
+        (dist > 0) && (dist <= a.width)
+      }
+    }
+  }
+
+  property("distance between filters should be symmetrical") {
+    forAll((a: bf.BF, b: bf.BF) => a.hammingDistance(b) == b.hammingDistance(a))
+  }
+
+  property("+ is the same as adding with create") {
+    forAll { (a: bf.BF, b: String) =>
+      Equiv[bf.BF].equiv(a + b, Monoid.plus(a, bf.create(b)))
+    }
+  }
+
+  property("maybeContains is consistent with contains") {
+    forAll((a: bf.BF, b: String) => a.maybeContains(b) == a.contains(b).isTrue)
+  }
+
+  property("after + maybeContains is true") {
+    forAll((a: bf.BF, b: String) => (a + b).maybeContains(b))
+  }
+
+  property("checkAndAdd works like check the add") {
+    forAll { (a: bf.BF, b: String) =>
+      val (next, check) = a.checkAndAdd(b)
+      val next1 = a + b
+
+      Equiv[bf.BF].equiv(next, next1) &&
+      (check == a.contains(b))
+    }
+  }
+
+  property("a ++ a = a for BF") {
+    forAll((a: bf.BF) => Equiv[bf.BF].equiv(a ++ a, a))
+  }
+}
+
+class ExperimentalBloomFilterHashIndices extends CheckProperties {
+
+  implicit val bf: Arbitrary[BloomFilter[String]] =
+    Arbitrary {
+      for {
+        hashes <- Gen.choose(1, 10)
+        width <- Gen.choose(100, 5000000)
+      } yield BloomFilter[String](hashes, width)
+    }
+
+  property("Indices are non negative") {
+    forAll((bf: BloomFilter[String], v: Long) => bf.Hash(v.toString).forall(e => e >= 0))
+  }
+
+  /**
+   *   This is the version of the Hash as of before the "negative values fix"
+   */
+  case class NegativeHash(numHashes: Int, width: Int) {
+    val size = numHashes
+
+    def apply(s: String): Stream[Int] = nextHash(s.getBytes, numHashes)
+
+    private def splitLong(x: Long) = {
+      val upper = math.abs(x >> 32).toInt
+      val lower = math.abs((x << 32) >> 32).toInt
+      (upper, lower)
+    }
+
+    private def nextHash(bytes: Array[Byte], k: Int, digested: Seq[Int] = Seq.empty): Stream[Int] =
+      if (k == 0)
+        Stream.empty
+      else {
+        val d = if (digested.isEmpty) {
+          val (a, b) = MurmurHash128(k)(bytes)
+          val (x1, x2) = splitLong(a)
+          val (x3, x4) = splitLong(b)
+          Seq(x1, x2, x3, x4)
+        } else
+          digested
+
+        Stream.cons(d(0) % width, nextHash(bytes, k - 1, d.drop(1)))
+      }
+  }
+
+  implicit val pairOfHashes: Arbitrary[(BloomFilter[String], NegativeHash)] =
+    Arbitrary {
+      for {
+        hashes <- Gen.choose(1, 10)
+        width <- Gen.choose(100, 5000000)
+      } yield (BloomFilter[String](hashes, width), NegativeHash(hashes, width))
+    }
+
+  property(
+    "Indices of the two versions of Hashes are the same, unless the first one contains negative index"
+  ) {
+    forAll { (pair: (BloomFilter[String], NegativeHash), v: Long) =>
+      val s = v.toString
+      val (bf, negativeHash) = pair
+      val indices = negativeHash.apply(s)
+      (indices == (bf.Hash(s).toStream)) || indices.exists(_ < 0)
+    }
+  }
+}
+
+class BloomFilterFalsePositives[T: Gen: Hash128](falsePositiveRate: Double) extends ApproximateProperty {
+
+  type Exact = Set[T]
+  type Approx = BloomFilter[T]#BF
+
+  type Input = T
+  type Result = Boolean
+
+  val maxNumEntries = 1000
+
+  def exactGenerator =
+    for {
+      numEntries <- Gen.choose(1, maxNumEntries)
+      set <- Gen.containerOfN[Set, T](numEntries, implicitly[Gen[T]])
+    } yield set
+
+  def makeApproximate(set: Set[T]) = {
+    val bfMonoid = BloomFilter[T](set.size, falsePositiveRate)
+
+    val values = set.toSeq
+    bfMonoid.create(values: _*)
+  }
+
+  def inputGenerator(set: Set[T]) =
+    for {
+      randomValues <- Gen.listOfN[T](set.size, implicitly[Gen[T]])
+      x <- Gen.oneOf((set ++ randomValues).toSeq)
+    } yield x
+
+  def exactResult(s: Set[T], t: T) = s.contains(t)
+
+  def approximateResult(bf: BloomFilter[T]#BF, t: T) = bf.contains(t)
+}
+
+class BloomFilterCardinality[T: Gen: Hash128] extends ApproximateProperty {
+
+  type Exact = Set[T]
+  type Approx = BloomFilter[T]#BF
+
+  type Input = Unit
+  type Result = Long
+
+  val maxNumEntries = 10000
+  val falsePositiveRate = 0.01
+
+  def exactGenerator =
+    for {
+      numEntries <- Gen.choose(1, maxNumEntries)
+      set <- Gen.containerOfN[Set, T](numEntries, implicitly[Gen[T]])
+    } yield set
+
+  def makeApproximate(set: Set[T]) = {
+    val bfMonoid = BloomFilter[T](set.size, falsePositiveRate)
+
+    val values = set.toSeq
+    bfMonoid.create(values: _*)
+  }
+
+  def inputGenerator(set: Set[T]) = Gen.const(())
+
+  def exactResult(s: Set[T], u: Unit) = s.size
+  def approximateResult(bf: BloomFilter[T]#BF, u: Unit) = bf.size
+}
+
+class ExperimentalBloomFilterProperties extends ApproximateProperties("BloomFilter") {
+  import ApproximateProperty.toProp
+
+  for (falsePositiveRate <- List(0.1, 0.01, 0.001)) {
+    property(s"has small false positive rate with false positive rate = $falsePositiveRate") = {
+      implicit val intGen = Gen.choose(1, 1000)
+      toProp(new BloomFilterFalsePositives[Int](falsePositiveRate), 50, 50, 0.01)
+    }
+  }
+
+  property("approximate cardinality") = {
+    implicit val intGen = Gen.choose(1, 1000)
+    toProp(new BloomFilterCardinality[Int], 50, 1, 0.01)
+  }
+}
+
+class ExperimentalBloomFilterTest extends AnyWordSpec with Matchers {
+
+  val RAND = new scala.util.Random
+
+  "BloomFilter" should {
+
+    "be possible to create from an iterator" in {
+      val bloomFilter = BloomFilter[String](RAND.nextInt(5) + 1, RAND.nextInt(64) + 32)
+      val entries = (0 until 100).map(_ => RAND.nextInt.toString)
+      val bf = bloomFilter.create(entries.iterator)
+      assert(bf.isInstanceOf[bloomFilter.BF])
+    }
+
+    "be possible to create from a sequence" in {
+      val bloomFilter = BloomFilter[String](RAND.nextInt(5) + 1, RAND.nextInt(64) + 32)
+      val entries = (0 until 100).map(_ => RAND.nextInt.toString)
+      val bf = bloomFilter.create(entries: _*)
+      assert(bf.isInstanceOf[bloomFilter.BF])
+    }
+
+    "identify all true positives" in {
+      (0 to 100).foreach { _ =>
+        val bloomFilter = BloomFilter[String](RAND.nextInt(5) + 1, RAND.nextInt(64) + 32)
+        val numEntries = 5
+        val entries = (0 until numEntries).map(_ => RAND.nextInt.toString)
+        val bf = bloomFilter.create(entries: _*)
+
+        entries.foreach { i =>
+          assert(bf.contains(i).isTrue)
+        }
+      }
+    }
+
+    "have small false positive rate" in {
+      val iter = 10000
+
+      Seq(0.1, 0.01, 0.001).foreach { fpProb =>
+        val fps = (0 until iter).map { _ =>
+          val numEntries = RAND.nextInt(10) + 1
+
+          val bfMonoid = BloomFilter[String](numEntries, fpProb)
+
+          val entries = RAND
+            .shuffle((0 until 1000).toList)
+            .take(numEntries + 1)
+            .map(_.toString)
+          val bf = bfMonoid.create(entries.drop(1): _*)
+
+          if (bf.contains(entries(0)).isTrue) 1.0 else 0.0
+        }
+
+        val observedFpProb = fps.sum / fps.size
+
+        // the 2.5 is a fudge factor to make the probability of it low
+        // in tests
+        assert(observedFpProb <= 2.5 * fpProb)
+      }
+    }
+
+    "approximate cardinality" in {
+      val bloomFilter = BloomFilter[String](10, 100000)
+
+      Seq(10, 100, 1000, 10000).foreach { exactCardinality =>
+        val items = (1 until exactCardinality).map(_.toString)
+        val bf = bloomFilter.create(items: _*)
+        val size = bf.size
+
+        assert(size ~ exactCardinality)
+        assert(size.min <= size.estimate)
+        assert(size.max >= size.estimate)
+      }
+    }
+
+    "work as an Aggregator" in {
+      (0 to 10).foreach { _ =>
+        val bloomFilter = BloomFilter[String](RAND.nextInt(5) + 1, RAND.nextInt(64) + 32)
+        import bloomFilter._
+
+        val numEntries = 5
+        val entries = (0 until numEntries).map(_ => RAND.nextInt.toString)
+        val bf = implicitly[Aggregator[String, bloomFilter.BF, bloomFilter.BF]].apply(entries)
+
+        entries.foreach(i => assert(bf.contains(i.toString).isTrue))
+      }
+    }
+
+    "not serialize @transient dense Instance" in {
+      val bloomFilter = BloomFilter[String](10, 0.1)
+
+      def serialize(bf: bloomFilter.BF): Array[Byte] = {
+        val stream = new ByteArrayOutputStream()
+        val out = new ObjectOutputStream(stream)
+        out.writeObject(bf)
+        out.close()
+        stream.close()
+        stream.toByteArray
+      }
+
+      val bf = bloomFilter.create((1 until 10).map(_.toString): _*)
+      val bytesBeforeSizeCalled = Bytes(serialize(bf))
+      val beforeSize = bf.size
+      assert(bf.contains("1").isTrue)
+      val bytesAfterSizeCalled = Bytes(serialize(bf))
+      assert(bytesBeforeSizeCalled.size == bytesAfterSizeCalled.size)
+      assert(beforeSize == bf.size)
+    }
+
+    /**
+     * this test failed before the fix for https://github.com/twitter/algebird/issues/229
+     */
+    "not have negative hash values" in {
+      val bfHash = BloomFilter[String](2, 4752800).Hash
+      val s = "7024497610539761509"
+      val index = bfHash.apply(s).head
+
+      assert(index >= 0)
+    }
+  }
+
+  "BloomFilter method `checkAndAdd`" should {
+
+    "be identical to method `+`" in {
+      (0 to 100).foreach { _ =>
+        val bloomFilter = BloomFilter[String](RAND.nextInt(5) + 1, RAND.nextInt(64) + 32)
+        import bloomFilter._
+
+        val numEntries = 5
+        val entries = (0 until numEntries).map(_ => RAND.nextInt.toString)
+        val bf = bloomFilter.create(entries: _*)
+        entries
+          .map(entry => (entry, bloomFilter.create(entry)))
+          .foldLeft((Monoid.zero, Monoid.zero)) {
+            case ((left, leftAlt), (entry, _)) =>
+              val (newLeftAlt, contained) = leftAlt.checkAndAdd(entry)
+              left.contains(entry) shouldBe contained
+              (left + entry, newLeftAlt)
+          }
+
+        entries.foreach(i => assert(bf.contains(i.toString).isTrue))
+      }
+    }
+  }
+
+  "BloomFilters" should {
+    "be able to compute Hamming distance to each other" in {
+      import BloomFilterTestUtils._
+
+      val bf = BloomFilter[String](3, 64)
+
+      val firstBloomFilter = bf.create(Seq("A").iterator)
+      val secondBloomFilter = bf.create(Seq("C").iterator)
+
+      val distance1 = firstBloomFilter.hammingDistance(secondBloomFilter)
+      assert(distance1 === 4)
+
+      val thirdBloomFilter = bf.create(Seq("A", "B", "C").iterator)
+      // Make it dense to make sure that that case is also covered
+      // even though these examples are small and thus sparse.
+      val forthBloomFilter = toDense(bf)(bf.create(Seq("C", "D", "E").iterator))
+
+      val distance2 = thirdBloomFilter.hammingDistance(forthBloomFilter)
+      assert(distance2 === 8)
+
+      val emptyBloomFilter = bf.create(Iterator.empty)
+      val distanceToEmpty = thirdBloomFilter.hammingDistance(emptyBloomFilter)
+      assert(distanceToEmpty === thirdBloomFilter.numBits)
+
+    }
+  }
+
+}
