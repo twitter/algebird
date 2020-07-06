68d9963d797e3bfa83fbfd67846ba72b39fbef85
Jeff Sarnat <jeffrey.sarnat@gmail.com>
2020-07-06 11:49:24 -0700

    most of the review feedback

--- algebird-core/src/main/scala/com/twitter/algebird/CorrelationGroup.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/CorrelationGroup.scala
@@ -1,10 +1,45 @@
 package com.twitter.algebird
 
 object Correlation {
+  def apply(x: (Double, Double), weight: Double): Correlation =
+    Correlation(c2 = 0, m2Left = 0, m2Right = 0, m1Left = x._1, m1Right = x._2, weight)
+
   def apply(x: (Double, Double)): Correlation =
-    Correlation(c2 = 0, m2Left = 0, m2Right = 0, m1Left = x._1, m1Right = x._2, 1L)
+    apply(x, 1.0)
 
   implicit val group: Group[Correlation] = CorrelationGroup
+
+  /**
+   * When combining averages, if the counts sizes are too close we
+   * should use a different algorithm.  This constant defines how
+   * close the ratio of the smaller to the total count can be:
+   */
+  private val STABILITY_CONSTANT = 0.1
+
+  /**
+   * Given two streams of doubles (weightN, an) and (weightK, ak) of form (weighted count,
+   * mean), calculates the mean of the combined stream.
+   *
+   * Uses a more stable online algorithm which should be suitable for
+   * large numbers of records similar to:
+   * http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+   *
+   * This differs from the implementation in MomentsGroup.scala only in that here, the counts are weighted, and are
+   * thus doubles instead of longs
+   */
+  def getCombinedMean(weightN: Double, an: Double, weightK: Double, ak: Double): Double =
+    if (weightN < weightK) getCombinedMean(weightK, ak, weightN, an)
+    else
+      (weightN + weightK) match {
+        case 0L                              => 0.0
+        case newCount if newCount == weightN => an
+        case newCount =>
+          val scaling = weightK.toDouble / newCount
+          // a_n + (a_k - a_n)*(k/(n+k)) is only stable if n is not approximately k
+          if (scaling < STABILITY_CONSTANT) (an + (ak - an) * scaling)
+          else (weightN * an + weightK * ak) / newCount
+      }
+
 }
 
 /**
@@ -22,29 +57,25 @@ case class Correlation(
     m2Right: Double,
     m1Left: Double,
     m1Right: Double,
-    m0: Long
+    m0: Double
 ) {
-  def count: Long = m0
+  def totalWeight: Double = m0
 
   def meanLeft: Double = m1Left
 
   def meanRight: Double = m1Right
 
-  // useful in calculating variance and stddev; private because we don't have skewness or kurtosis info
-  private[algebird] def leftMoment: Moments = Moments(m0 = m0, m1 = m1Left, m2 = m2Left, 0, 0)
-
-  private[algebird] def rightMoment: Moments = Moments(m0 = m0, m1 = m1Right, m2 = m2Right, 0, 0)
-
   // variance, stddev, and covariance are for the population, not a sample
-  def varianceLeft: Double = leftMoment.variance
 
-  def varianceRight: Double = rightMoment.variance
+  def varianceLeft: Double = m2Left / m0
 
-  def stddevLeft: Double = leftMoment.stddev
+  def varianceRight: Double = m2Right / m0
 
-  def stddevRight: Double = rightMoment.stddev
+  def stddevLeft: Double = Math.sqrt(varianceLeft)
 
-  def covariance: Double = c2 / count
+  def stddevRight: Double = Math.sqrt(varianceRight)
+
+  def covariance: Double = c2 / totalWeight
 
   /**
    *
@@ -55,6 +86,20 @@ case class Correlation(
     // however, dividing by "count" cancels out, and leaves us with the following formula, which relies on fewer
     // divisions
     c2 / (Math.sqrt(m2Left * m2Right))
+
+  /**
+   * Assume this instance of Correlation came from summing together Correlation.apply((x_i, y_i)) for i in 1...n.
+   * @return (m, b) where y = mx + b is the line with the least squares fit of the points (x_i, y_i).
+   *         See, e.g. https://mathworld.wolfram.com/LeastSquaresFitting.html.
+   */
+  def linearLeastSquares: (Double, Double) = {
+    val m = c2 / m2Left
+    val b = meanRight - m * meanLeft
+    (m, b)
+  }
+
+  def swap: Correlation =
+    Correlation(c2 = c2, m2Left = m2Right, m2Right = m2Left, m1Left = m1Right, m1Right = m1Left, m0 = m0)
 }
 
 object CorrelationGroup extends Group[Correlation] {
@@ -64,15 +109,21 @@ object CorrelationGroup extends Group[Correlation] {
    * Pébay, Philippe (2008), "Formulas for Robust, One-Pass Parallel Computation of Covariances and Arbitrary-Order Statistical Moments",
    *   Technical Report SAND2008-6212, Sandia National Laboratories
    * https://prod-ng.sandia.gov/techlib-noauth/access-control.cgi/2008/086212.pdf
+   *
+   * Extending this to weights can be found in
+   * Schubert, Erich; Gertz, Michael (9 July 2018). Numerically stable parallel computation of (co-)variance.
+   *   ACM. p. 10. doi:10.1145/3221269.3223036. ISBN 9781450365055.
+   *   http://dl.acm.org/citation.cfm?id=3221269.3223036
+   *   https://dl.acm.org/doi/10.1145/3221269.3223036
    */
   override def plus(a: Correlation, b: Correlation): Correlation = {
-    val count = a.count + b.count
+    val count = a.totalWeight + b.totalWeight
     if (count == 0)
       zero
     else {
-      val prodSumRatio = a.count.toDouble * b.count / count
-      val m1Left = MomentsGroup.getCombinedMean(a.count, a.m1Left, b.count, b.m1Left)
-      val m1Right = MomentsGroup.getCombinedMean(a.count, a.m1Right, b.count, b.m1Right)
+      val prodSumRatio = a.totalWeight * b.totalWeight / count
+      val m1Left = Correlation.getCombinedMean(a.totalWeight, a.m1Left, b.totalWeight, b.m1Left)
+      val m1Right = Correlation.getCombinedMean(a.totalWeight, a.m1Right, b.totalWeight, b.m1Right)
       val deltaLeft = b.m1Left - a.m1Left
       val deltaRight = b.m1Right - a.m1Right
 
@@ -104,4 +155,11 @@ object CorrelationAggregator extends MonoidAggregator[(Double, Double), Correlat
   override def prepare(a: (Double, Double)): Correlation = Correlation(a)
   override val monoid = CorrelationGroup
   override def present(c: Correlation): Correlation = c
+
+  def correlation: MonoidAggregator[(Double, Double), Correlation, Double] =
+    this.andThenPresent(_.correlation)
+
+  def covariance: MonoidAggregator[(Double, Double), Correlation, Double] =
+    this.andThenPresent(_.covariance)
+
 }
--- algebird-test/src/test/scala/com/twitter/algebird/CorrelationLaws.scala
+++ algebird-test/src/test/scala/com/twitter/algebird/CorrelationLaws.scala
@@ -1,12 +1,34 @@
 package com.twitter.algebird
 
-import org.scalatest._
 import org.scalacheck.Prop.forAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 import com.twitter.algebird.BaseProperties._
 import com.twitter.algebird.scalacheck.arbitrary._
 
-class CorrelationLaws extends CheckProperties {
+object CorrelationLaws {
   val EPS = 1e-10
+
+  def aggregateFunction(f: Double => Double): Aggregator[Double, Correlation, Double] =
+    CorrelationAggregator
+      .composePrepare[Double](x => (x, f(x)))
+      .andThenPresent(_.correlation)
+
+  val testList = Range.inclusive(-10, 10).map(_.toDouble).toList
+
+  def corrApproxEq(corr1: Correlation, corr2: Correlation): Boolean =
+    approxEqOrBothNaN(EPS)(corr1.c2, corr2.c2) &&
+      approxEqOrBothNaN(EPS)(corr1.m2Left, corr2.m2Left) &&
+      approxEqOrBothNaN(EPS)(corr1.m2Right, corr2.m2Right) &&
+      approxEqOrBothNaN(EPS)(corr1.m1Left, corr2.m1Left) &&
+      approxEqOrBothNaN(EPS)(corr1.m1Right, corr2.m1Right) &&
+      approxEqOrBothNaN(EPS)(corr1.m0, corr2.m0)
+
+}
+
+class CorrelationLaws extends CheckProperties {
+
+  import CorrelationLaws._
 
   property("Correlation group laws") {
     implicit val equiv: Equiv[Correlation] =
@@ -22,39 +44,56 @@ class CorrelationLaws extends CheckProperties {
   }
 
   property("Central moments 0 through 2 match implementation from Moments") {
-    forAll { (corr1: Correlation, corr2: Correlation) =>
-      val corr = Monoid.plus(corr1, corr2)
-      val leftMoment = Monoid.plus(corr1.leftMoment, corr2.leftMoment)
-      val rightMoment = Monoid.plus(corr1.rightMoment, corr2.rightMoment)
-      corr.count == leftMoment.count &&
-      corr.count == rightMoment.count &&
+    forAll { (l: List[(Double, Double)]) =>
+      val corr = Monoid.sum(l.map(Correlation.apply))
+      val leftMoment = Monoid.sum(l.map { case (x, _) => Moments.apply(x) })
+      val rightMoment = Monoid.sum(l.map { case (_, y) => Moments.apply(y) })
+      approxEq(EPS)(corr.totalWeight, leftMoment.count) &&
+      approxEq(EPS)(corr.totalWeight, rightMoment.count) &&
       approxEq(EPS)(corr.meanLeft, leftMoment.mean) &&
       approxEq(EPS)(corr.meanRight, rightMoment.mean) &&
-      approxEq(EPS)(corr.stddevLeft, leftMoment.stddev) &&
-      approxEq(EPS)(corr.stddevRight, rightMoment.stddev)
+      (l.length < 2 ||
+      (approxEqOrBothNaN(EPS)(corr.stddevLeft, leftMoment.stddev) &&
+      approxEqOrBothNaN(EPS)(corr.stddevRight, rightMoment.stddev)))
+    }
+  }
 
+  property("central moments of a line return the slope of the line") {
+    // using arbitrary floating point values for this test yields far less numerical precision
+    forAll { (m: Int, b: Int) =>
+      val calculatedCorrelation = aggregateFunction(x => m * x + b)(testList)
+      (m == 0.0
+      || (m > 0.0 && approxEq(EPS)(calculatedCorrelation, 1.0))
+      || (m < 0.0 && approxEq(EPS)(calculatedCorrelation, -1.0)))
+    }
+  }
+
+  property("given points exactly on a straight line, least squares approximation finds slope and intercept") {
+    // using arbitrary floating point values for this test yields far less numerical precision
+    forAll { (m: Int, b: Int) =>
+      val (mOut, bOut) =
+        CorrelationAggregator
+          .composePrepare[Double](x => (x, m * x + b))
+          .andThenPresent(_.linearLeastSquares)(testList)
+
+      approxEq(EPS)(m.toDouble, mOut) && approxEq(EPS)(b.toDouble, bOut)
+    }
+  }
+
+  property("the swap method on moments works as you'd think") {
+    forAll { l: List[(Double, Double)] =>
+      val swapped = CorrelationAggregator(l).swap
+      val reversedInput = CorrelationAggregator.composePrepare[(Double, Double)] { case (x, y) => (y, x) }(l)
+      corrApproxEq(swapped, reversedInput)
     }
   }
 }
 
-class CorrelationTest extends WordSpec with Matchers {
-  def aggregateFunction(f: Double => Double): Aggregator[Double, Correlation, Double] =
-    CorrelationAggregator
-      .composePrepare[Double](x => (x, f(x)))
-      .andThenPresent(_.correlation)
-
-  val testList = Range.inclusive(-10, 10).map(_.toDouble).toList
-
-  "correlation with y = x should be 1" in {
-    aggregateFunction(identity)(testList) shouldEqual (1.0)
-  }
-
-  "correlation with y = -x should be -1" in {
-    aggregateFunction(x => -x)(testList) shouldEqual (-1.0)
-  }
+class CorrelationTest extends AnyWordSpec with Matchers {
+  import CorrelationLaws._
 
   "correlation with y = x*x should be 0" in {
-    aggregateFunction(x => x * x) should equal(0.0)
+    aggregateFunction(x => x * x)(testList) should equal(0.0)
   }
 
 }
